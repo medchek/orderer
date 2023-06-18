5c4a1b4a3c604bf48bcaaa2f2747a70f459064ef
CHMD <wizzsha@gmail.com>
2023-06-18 16:16:39 +0100

    feat: add slices to the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,12 @@
+import { OrderFormSlice, orderFormSlice } from "./orderFormSlice";
 import { create } from "zustand";
 import { ProductSlice, productSlice } from "./productSlice";
+import { WilayaSlice, uiSlice } from "./wilayaSlice";
 
-export const useStore = create<ProductSlice>()((...args) => ({
-  ...productSlice(...args),
-}));
+export const useStore = create<ProductSlice & WilayaSlice & OrderFormSlice>()(
+  (...args) => ({
+    ...productSlice(...args),
+    ...uiSlice(...args),
+    ...orderFormSlice(...args),
+  })
+);

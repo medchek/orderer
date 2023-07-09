17eb69a06a398a9700a20e1a9c9a066cca3caa7d
CHMD <wizzsha@gmail.com>
2023-07-09 19:59:08 +0100

    feat: add util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
 import { customAlphabet } from "nanoid";
+import { Session } from "next-auth";
+// import { prisma } from "../../prisma/db";
 
 const randId = (max: number = 1000) => Math.ceil(Math.random() * max);
 
@@ -25,4 +27,44 @@ export const uniqueId = (len = 20): string => {
   const id = customAlphabet(alphabet, len);
 
   return id();
+};
+
+/**
+ * Return whether the session belongs to the admin
+ * @param session
+ * @returns
+ */
+export const isAdmin = (session: Session | null) => {
+  return session && session.user?.email === process.env.GOOGLE_ADMIN_EMAIL;
+};
+
+/**
+ * Used to simulate network latency
+ */
+export const sleep = (timeInMs: number): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeInMs);
+  });
+};
+
+/**
+ * Converts a number string to a number
+ * @param n String number
+ * @returns The converted number. If the provided string was not a valid number returns 0
+ */
+export const toNumber = (n: string) => {
+  const converted = parseInt(n.trim());
+  return !isNaN(converted) && isFinite(converted) ? converted : 0;
+};
+
+/**
+ * Converts a number string to a number
+ * @param n The string number
+ * @returns returns the converted number, if a wrrong number is supplied or "0" returns null
+ */
+export const toNumberOrNull = (n: string): number | null => {
+  const converted = parseInt(n.trim());
+  return !isNaN(converted) && isFinite(converted) && converted !== 0
+    ? converted
+    : null;
 };

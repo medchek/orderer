3e063af927972f2ccc6ca8112c69e886cd8d382d
CHMD <wizzsha@gmail.com>
2023-07-13 19:37:46 +0100

    feat: add account actions and lazy loading

--- src/components/dashboard/DashboardProductsToolbar.tsx
+++ src/components/dashboard/DashboardProductsToolbar.tsx
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
+"use client";
+import React, { Fragment, useState } from "react";
 import { MdAdd } from "react-icons/md";
-import DashboardAddProduct from "./DashboardAddProduct";
+import AccountActions from "../AccountActions";
+import dynamic from "next/dynamic";
+import ModalLoader from "../ModalLoader";
+
+const DashboardAddProduct = dynamic(() => import("./DashboardAddProduct"), {
+  loading: () => <ModalLoader />,
+});
 
 export const DashboardProductsToolbar = () => {
   const [isAddProductOpen, setIsAddProductOpen] = useState(false);
 
   return (
-    <div id="dashboard-tools" className="h-14 min-h-[3.5rem]">
-      <button
-        type="button"
-        className="flex h-10 items-center justify-center gap-1 rounded-md bg-primary px-4 font-semibold text-white transition-colors dark:bg-card-dark dark:hover:bg-[#242436] dark:focus:bg-[#0c0c13]"
-        onClick={() => setIsAddProductOpen(true)}
+    <Fragment>
+      <div
+        id="dashboard-title"
+        className="flex h-14 min-h-[3.5rem] w-full justify-between"
       >
-        <MdAdd className="h-6 w-6" /> Ajouter un Produit
-      </button>
-      {isAddProductOpen && (
-        <DashboardAddProduct closeModal={() => setIsAddProductOpen(false)} />
-      )}
-    </div>
+        <h1 className="text-2xl font-bold dark:text-white">Products</h1>
+        <AccountActions />
+      </div>
+      <div id="dashboard-tools" className="h-14 min-h-[3.5rem]">
+        <button
+          type="button"
+          className="flex h-10 items-center justify-center gap-1 rounded-md bg-primary px-4 font-semibold text-white transition-colors dark:bg-card-dark dark:hover:bg-[#242436] dark:focus:bg-[#0c0c13]"
+          onClick={() => setIsAddProductOpen(true)}
+        >
+          <MdAdd className="h-6 w-6" /> Ajouter un Produit
+        </button>
+        {isAddProductOpen && (
+          <DashboardAddProduct closeModal={() => setIsAddProductOpen(false)} />
+        )}
+      </div>
+    </Fragment>
   );
 };

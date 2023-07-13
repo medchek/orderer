6bd198ecdfce98cd74b529ba3448914e9795dc9b
CHMD <wizzsha@gmail.com>
2023-07-13 19:38:08 +0100

    feat: add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const PRODUCT_CODE_LENGTH = 20;
+export const MAX_UPLOAD_FILE_SIZE = 8388608;
+
+export const STATUS_CREATED = 201;
+export const STATUS_OK = 200;
+export const STATUS_CONFLICT = 409;
+export const STATUS_NOT_FOUND = 404;
+export const STATUS_UNAUTHORIZED = 401;
+export const STATUS_BAD_REQUEST = 400;
+export const STATUS_FORBIDDEN = 403;
+export const STATUS_CONTENT_TOO_LARGE = 413;

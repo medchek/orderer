8a2522562b4d54d7a9bcc67ce69a7f0dacb161b6
CHMD <wizzsha@gmail.com>
2023-07-13 18:47:42 +0100

    style: change snackbar default colors

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -24,8 +24,10 @@ export default function Snackbar({}: Props) {
   return createPortal(
     <div
       id="snack"
-      className={`absolute bottom-6 left-0 right-0 z-50 mx-auto flex h-14 w-96 items-center justify-between rounded-xl px-4 text-sm  shadow-lg dark:text-card-dark ${
-        stanckType === "error" ? "dark:bg-red-700" : "bg-[#f6f7fd]"
+      className={`absolute bottom-6 left-0 right-0 z-50 mx-auto flex h-14 w-96 items-center justify-between rounded-xl px-4 text-sm  shadow-lg  ${
+        stanckType === "error"
+          ? "text-white dark:bg-red-700"
+          : "bg-[#e9e9ff] dark:text-card-dark"
       }`}
     >
       <p>{snackText}</p>
@@ -34,11 +36,11 @@ export default function Snackbar({}: Props) {
         className={`h-8 min-h-[2rem] w-8 min-w-[2rem] rounded-md transition-colors ${
           stanckType === "error"
             ? "dark:hover:bg-white/25 dark:focus:bg-white/[.15]"
-            : "dark:hover:bg-white/10 dark:focus:bg-white/5"
+            : "dark:hover:bg-card-dark/10 dark:focus:bg-card-dark/20"
         }`}
         onClick={hideSnackbar}
       >
-        <MdClear className="h-6 w-6" onClick={close} />
+        <MdClear className="h-6 w-6" />
       </button>
     </div>,
     document.body

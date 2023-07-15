078d576bcccecf7847e786b10d5fc1c500432059
CHMD <wizzsha@gmail.com>
2023-07-15 16:08:47 +0100

    feat: add dahsboard header

---
+++ src/components/dashboard/DashboardHeader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AccountActions from "../AccountActions";
+
+interface Props {
+  label: string;
+}
+
+export default function DashboardHeader({ label }: Props) {
+  return (
+    <div
+      id="dashboard-title"
+      className="flex h-14 min-h-[3.5rem] w-full justify-between pr-6"
+    >
+      <h1 className="text-2xl font-bold dark:text-white">{label}</h1>
+      <AccountActions />
+    </div>
+  );
+}

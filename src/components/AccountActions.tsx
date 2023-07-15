34aaca6bc39e0a7f0345b0b4891eb70b86982734
CHMD <wizzsha@gmail.com>
2023-07-15 16:08:28 +0100

    feat: update account actions component based on session state

--- src/components/AccountActions.tsx
+++ src/components/AccountActions.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { signOut, useSession } from "next-auth/react";
 import React, { useRef, useState } from "react";
-import { IoLogOutOutline } from "react-icons/io5";
+import { IoLogInOutline, IoLogOutOutline } from "react-icons/io5";
 import Loader from "./Loader";
 import { useOnClickOutside } from "usehooks-ts";
+import Link from "next/link";
 
 interface Props {
   className?: string;
@@ -23,30 +25,52 @@ export default function AccountActions({ className }: Props) {
     setIsOpen(!isOpen);
   };
 
-  const test = async () => {
-    const r = fetch("/api/images", { method: "GET" });
-  };
-  return (
-    <div
-      id="account-actions"
-      className={`relative z-10 h-10 w-10 ${className ? className : "mr-6"}`}
-    >
-      <button
-        type="button"
-        className="h-11 w-11 overflow-hidden rounded-full dark:bg-card-dark dark:hover:bg-[#262638]"
-        onClick={handleOpenMenu}
-      >
-        {status === "loading" ? (
+  const buttonContent = () => {
+    if (status === "loading") {
+      return (
+        <div className="flex h-11 w-11 items-center justify-center overflow-hidden rounded-full dark:bg-card-dark">
           <Loader className="h-6 w-6" />
-        ) : (
+        </div>
+      );
+    } else if (status === "unauthenticated") {
+      return (
+        <Link
+          type="button"
+          className="relative z-10 flex h-9 w-auto items-center justify-center rounded-lg px-2 text-[#535663] transition-colors dark:bg-[#101016]  dark:hover:bg-[#14151b] dark:focus:bg-[#0b0b0e]"
+          href="/login"
+          title="Se connecter"
+        >
+          <span>connexion</span>
+          {/* <IoLogInOutline className="h-7 w-7" /> */}
+        </Link>
+      );
+    } else {
+      return (
+        <button
+          type="button"
+          className="h-11 w-11 overflow-hidden rounded-full dark:bg-card-dark dark:hover:bg-[#262638]"
+          onClick={handleOpenMenu}
+          disabled={status !== "authenticated"}
+        >
           <img
             src={data?.user?.image!}
             className="h-9 w-9 rounded-full"
             alt="profile-image"
             loading="lazy"
+            referrerPolicy="no-referrer"
           />
-        )}
-      </button>
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div
+      id="account-actions"
+      className={`relative z-10 h-10 w-auto ${className && className}`}
+    >
+      {buttonContent()}
+
       {isOpen && (
         <div
           className="absolute right-0 mt-2 w-48 rounded-lg bg-card-dark p-2 text-white shadow-lg"

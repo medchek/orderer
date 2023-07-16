afa0be17baadbdec39d017bd3fc219a75f2b1710
CHMD <wizzsha@gmail.com>
2023-07-16 19:05:46 +0100

    feat: add nav links

--- src/components/dashboard/DashboardNav.tsx
+++ src/components/dashboard/DashboardNav.tsx
@@ -5,22 +5,27 @@ import React from "react";
 type Props = {};
 
 export default function DashboardNav({}: Props) {
-  const links = ["products", "orders"];
+  const links = [
+    { name: "Produits", href: "" },
+    { name: "Commandes", href: "orders" },
+    { name: "Prix de livraisons", href: "shipping-prices" },
+    { name: "Liste noire", href: "blacklist" },
+  ];
 
-  const LinkList = links.map((link) => (
+  const LinkList = links.map(({ href, name }) => (
     <Link
-      className="flex h-[50px] w-full items-center rounded-lg bg-[#EC2A2A] pl-5  font-semibold capitalize text-white transition-colors hover:bg-[#ff4545] focus:bg-[#da2020] dark:bg-[#22233f] dark:hover:bg-[#2f325f] dark:focus:bg-[#070714]"
-      key={link}
-      href={`#${link}`}
+      className="flex h-[50px] w-full items-center rounded-lg bg-[#EC2A2A] pl-5  font-semibold text-white transition-colors first-letter:capitalize hover:bg-[#ff4545] focus:bg-[#da2020] dark:bg-[#22233f] dark:hover:bg-[#2f325f] dark:focus:bg-[#070714]"
+      key={href}
+      href={`/dashboard/${href}`}
     >
-      {link}
+      {name}
     </Link>
   ));
   return (
     <div
       id="dashboard-nav"
       // dark:bg-[#0b0c1f]
-      className="flex h-full w-[335px] min-w-[335px] flex-col justify-between bg-primary-darker px-5 py-2 dark:bg-card-dark"
+      className="flex h-full min-w-[280px] flex-col justify-between bg-primary-darker px-5 py-2 dark:bg-card-dark 2xl:min-w-[335px]"
     >
       <section className="flex grow  flex-col">
         <div id="nav-logo" className="flex w-[55px] flex-col items-center">
@@ -40,7 +45,7 @@ export default function DashboardNav({}: Props) {
       </section>
       <p
         id="nav-legal"
-        className="w-full text-center text-[#ffc0c0] dark:text-[#646479]"
+        className="w-full text-center text-sm text-[#ffc0c0] dark:text-[#646479]"
       >
         Copyright © {new Date().getFullYear()} TRB Eshop
       </p>

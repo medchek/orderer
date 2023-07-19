2625ce124ca825dd676bc48688d0f6f37310ca97
CHMD <wizzsha@gmail.com>
2023-07-19 20:31:05 +0100

    feat: add prop to disable preset dashboard header padding

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -3,13 +3,16 @@ import AccountActions from "../AccountActions";
 
 interface Props {
   label: string;
+  noPadding?: boolean;
 }
 
-export default function DashboardHeader({ label }: Props) {
+export default function DashboardHeader({ label, noPadding }: Props) {
   return (
     <div
       id="dashboard-title"
-      className="flex h-14 min-h-[3.5rem] w-full justify-between pr-6"
+      className={`flex h-14 min-h-[3.5rem] w-full justify-between ${
+        !noPadding && "pr-6"
+      }`}
     >
       <h1 className="text-xl font-bold dark:text-white">{label}</h1>
       <AccountActions />

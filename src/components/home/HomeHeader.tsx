c58a0f6b6bf5829b9c4324c2765bee15a08da1a1
CHMD <wizzsha@gmail.com>
2023-07-28 22:19:39 +0100

    feat: update logo postion

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -2,22 +2,23 @@
 import Image from "next/image";
 import React from "react";
 import AccountActions from "../AccountActions";
+import Link from "next/link";
 
 type Props = {};
 
 export default function HomeHeader({}: Props) {
   return (
-    <header className="relative mx-10 flex h-20 w-full items-start justify-end pt-2 2xl:mx-64">
+    <header className="relative flex h-20 w-full items-start justify-between pt-2">
       {/* absolute content */}
-      <div
+      {/* <div
         id="header-logo"
         className="absolute left-0 right-0 flex h-14 w-full items-center justify-center"
-      >
-        <div className="relative h-14 w-14">
-          <Image src="/trb-logo.png" alt="TRB Eshop Logo" fill priority />
-        </div>
-        {/* <p className="text-center text-sm font-semibold text-primary">E-Shop</p> */}
-      </div>
+       >*/}
+      <Link className="relative h-12 w-12" href=".">
+        <Image src="/trb-logo.png" alt="TRB Eshop Logo" fill priority />
+      </Link>
+      {/* <p className="text-center text-sm font-semibold text-primary">E-Shop</p> */}
+      {/* </div> */}
       {/* relative content */}
       <AccountActions className="mr-0" />
     </header>

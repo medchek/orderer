607296bcd071ecec30de533b32859e181ac58d91
CHMD <wizzsha@gmail.com>
2023-08-05 18:37:49 +0100

    style: smaller text on smaller viewports

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -25,7 +25,7 @@ export default function Input({
     <div className="mb-1 flex w-full flex-col">
       <label
         htmlFor={inputId}
-        className="mb-1 text-lg font-semibold dark:text-stone-100"
+        className="mb-1 2xl:text-lg font-semibold dark:text-stone-100"
       >
         {label}
       </label>
@@ -33,7 +33,7 @@ export default function Input({
         id={inputId}
         {...props}
         {...(register && register(name, registerRules))}
-        className="h-12 rounded-lg bg-[#ECECEC] px-4 placeholder-[#979797] outline-none ring-secondary focus:ring-2 disabled:cursor-not-allowed disabled:text-stone-400 dark:bg-input-dark dark:text-white dark:[color-scheme:dark]"
+        className="h-12 rounded-lg bg-[#ECECEC] px-4 placeholder-[#979797] outline-none ring-secondary focus:ring-2 disabled:cursor-not-allowed disabled:text-stone-400 dark:bg-input-dark dark:text-white dark:[color-scheme:dark] text-sm 2xl:text-base"
       />
       <div className="h-5 text-sm text-red-600 dark:text-red-500">
         {error && error}

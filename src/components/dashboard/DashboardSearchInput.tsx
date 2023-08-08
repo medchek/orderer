402a7dc551cc84f1326d0ebe8c95736855da55dc
CHMD <wizzsha@gmail.com>
2023-08-08 18:34:07 +0100

    style: update input dark color

--- src/components/dashboard/DashboardSearchInput.tsx
+++ src/components/dashboard/DashboardSearchInput.tsx
@@ -1,15 +1,16 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 import { MdSearch } from "react-icons/md";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export default function DashboardSearchInput({ ...props }: Props) {
+export default function DashboardSearchInput({ className, ...props }: Props) {
   return (
-    <div className="relative flex h-10 w-64 items-center">
+    <div className={cn("relative flex h-10 w-64 items-center", className)}>
       <MdSearch className="absolute left-2 h-6 w-6 text-stone-400" />
       <input
         type="search"
-        className="h-full w-full rounded-lg bg-[#ECECEC] px-4 pl-10 pr-4 text-sm placeholder-[#979797] outline-none ring-secondary focus:ring-2 dark:bg-[#17181D] dark:text-white dark:[color-scheme:dark] "
+        className="h-full w-full rounded-lg bg-[#ECECEC] pl-10 pr-2 text-sm placeholder-[#979797] outline-none ring-secondary focus:ring-2 dark:bg-neutral-900 dark:text-white dark:[color-scheme:dark] "
         {...props}
       />
     </div>

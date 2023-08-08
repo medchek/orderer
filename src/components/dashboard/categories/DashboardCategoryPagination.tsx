16e62baeaa05371eaa69c6cc3bb982f9885acd46
CHMD <wizzsha@gmail.com>
2023-08-08 18:35:03 +0100

    feat: add ability to select the number of displayed categories per page

--- src/components/dashboard/categories/DashboardCategoryPagination.tsx
+++ src/components/dashboard/categories/DashboardCategoryPagination.tsx
@@ -1,3 +1,5 @@
+import { toNumber } from "@/lib/utils";
+import { useStore } from "@/store";
 import React, { useState } from "react";
 import { MdChevronRight } from "react-icons/md";
 import ReactPaginate, { type ReactPaginateProps } from "react-paginate";
@@ -5,8 +7,16 @@ import ReactPaginate, { type ReactPaginateProps } from "react-paginate";
 interface Props extends ReactPaginateProps {}
 
 export default function DashboardCategoryPagination({ ...props }: Props) {
+  const { categoryPerPage, setCategoryPerPage } = useStore();
+
+  const perPageOptions = [5, , 7, 9, 10, 15, 20, 25];
+  const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = toNumber(e.target.value);
+    setCategoryPerPage(value);
+  };
+
   return (
-    <div className="h-14 min-h-[3.5rem] w-full flex items-center justify-center">
+    <div className="relative h-14 min-h-[3.5rem] w-full flex items-center justify-center">
       <div>
         <ReactPaginate
           {...props}
@@ -14,15 +24,34 @@ export default function DashboardCategoryPagination({ ...props }: Props) {
           breakLabel="..."
           nextLabel={<MdChevronRight className="w-7 h-7" />}
           previousLabel={<MdChevronRight className="w-7 h-7 rotate-180 " />}
-          nextLinkClassName="flex justify-center items-center w-8 h-8 bg-stone-950 hover:bg-stone-800  rounded-md"
-          previousLinkClassName="flex justify-center items-center w-8 h-8 bg-stone-950 hover:bg-stone-800  rounded-md"
-          breakLinkClassName="flex font-bold justify-center items-center w-8 h-8 bg-stone-950 hover:bg-stone-800  rounded-md"
-          pageLinkClassName="w-8 h-8 bg-stone-950 hover:bg-stone-800  rounded-md font-semibold flex justify-center items-center"
+          nextLinkClassName="flex justify-center items-center w-10 h-10 bg-stone-950 hover:bg-stone-800  rounded-md"
+          previousLinkClassName="flex justify-center items-center w-10 h-10 bg-stone-950 hover:bg-stone-800  rounded-md"
+          breakLinkClassName="flex font-bold justify-center items-center w-10 h-10 bg-stone-950 hover:bg-stone-800  rounded-md"
+          pageLinkClassName="w-10 h-10 bg-stone-950 hover:bg-stone-800  rounded-md font-semibold flex justify-center items-center"
           disabledLinkClassName="opacity-40 hover:!bg-stone-950 cursor-not-allowed"
           activeLinkClassName="!bg-secondary"
           pageRangeDisplayed={3}
           // marginPagesDisplayed={2}
+          renderOnZeroPageCount={null}
+          // onPageChange={(e) => {
+          //   console.log("chaned!", e.selected);
+          // }}
         />
+      </div>
+      <div className="absolute text-sm text-stone-400 h-10 right-0 flex items-center gap-2">
+        <span>Afficher</span>
+        <select
+          className="bg-neutral-900 h-10 px-1 text-stone-200 rounded-md flex items-center justify-center text-base"
+          value={categoryPerPage}
+          onChange={handleOnChange}
+        >
+          {perPageOptions.map((v) => (
+            <option key={v} value={v}>
+              {v}
+            </option>
+          ))}
+        </select>
+        <span>catégories</span>
       </div>
     </div>
   );

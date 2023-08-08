52d0b57747d57f03a757cb8db56a38fb1cdb58e8
CHMD <wizzsha@gmail.com>
2023-08-08 18:41:51 +0100

    feat: add category filtering global state

--- src/store/dashboardSlice.ts
+++ src/store/dashboardSlice.ts
@@ -11,18 +11,38 @@ export interface CategoryDataOpen {
   id: number;
 }
 export interface DashboardSlice {
+  // products
   isAddProductOpen: boolean;
+  setIsAddProductOpen: (v: boolean) => void
+
+  // Categories
   deleteCategoryData: CategoryDataClosed | CategoryDataOpen
   setDeleteCategoryData: (v: CategoryDataClosed | CategoryDataOpen) => void;
   editCategoryData: CategoryDataClosed | CategoryDataOpen;
   setEditCategoryData: (v: CategoryDataClosed | CategoryDataOpen) => void;
+
+  categoryPerPage: number;
+  /** Sets the number of categories that should be displayed per page */
+  setCategoryPerPage: (n: number) => void;
+
+  categoryFilterTerm: string;
+  categoryFilterType: CategoryType;
+
+  setCategoryFilterType: (v: CategoryType) => void
+  /** The provided value will be trimmed and lowercased */
+  setCategoryFilterTerm: (v: string) => void
+
+
 }
 
 export const dashboardSlice: StateCreator<DashboardSlice> = (set) => ({
+  // Products
   isAddProductOpen: false,
   setIsAddProductOpen: (v: boolean) => {
     set(() => ({ isAddProductOpen: v }))
   },
+
+  // Categories
   deleteCategoryData: { isOpen: false },
   setDeleteCategoryData: (data) => {
     set(() => ({ deleteCategoryData: data }))
@@ -30,6 +50,20 @@ export const dashboardSlice: StateCreator<DashboardSlice> = (set) => ({
   editCategoryData: { isOpen: false },
   setEditCategoryData: (data) => {
     set(() => ({ editCategoryData: data }))
+  },
+  categoryPerPage: 5,
+  setCategoryPerPage: (n) => {
+    set(() => ({ categoryPerPage: n }))
+  },
+  categoryFilterTerm: "",
+  categoryFilterType: "category",
+  setCategoryFilterTerm: (v) => {
+    set(() => ({ categoryFilterTerm: v.trim().toLowerCase() }))
+  },
+  setCategoryFilterType: (v) => {
+    set(() => ({ categoryFilterType: v, categoryFilterTerm: "" }))
+
   }
+
 
 });

fb849446d72b41e4070f24cb60d25f6aa702bc48
CHMD <wizzsha@gmail.com>
2023-08-08 18:28:32 +0100

    feat: order api towns by name

--- src/app/api/towns/[code]/route.ts
+++ src/app/api/towns/[code]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import { prisma } from "../../../../../prisma/db"
-import { STATUS_BAD_REQUEST, STATUS_OK } from "@/lib/constants"
-import { apiErrorResponse, toNumber } from "@/lib/utils"
+import { STATUS_BAD_REQUEST, STATUS_NOT_FOUND, STATUS_OK } from "@/lib/constants"
+import { apiErrorResponse, isNotFoundPrismaError, toNumber } from "@/lib/utils"
 
 export async function GET(req: NextRequest, { params }: { params: { code: string } }) {
   try {
@@ -13,11 +13,19 @@ export async function GET(req: NextRequest, { params }: { params: { code: string
     }
 
     const towns = await prisma.town.findMany({
-      where: { wilayaCode: toNumber(params.code) }
+      where: { wilayaCode: toNumber(params.code), }, orderBy: {
+        name: "asc"
+      }
     })
 
     return NextResponse.json(towns, { status: STATUS_OK })
   } catch (error) {
+
+    if (isNotFoundPrismaError(error)) {
+      return apiErrorResponse("Wrong wilaya code", STATUS_NOT_FOUND)
+
+    }
+
     console.error("Error getting tows of wilaya:", error)
     return apiErrorResponse("Couln't get towns")
   }

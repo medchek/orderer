a9fded7732f67e2cd2f5703abbf9b1f5d43514ab
medchek <wizzsha@gmail.com>
2023-08-28 18:41:30 +0100

    chore: add store devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,12 +5,22 @@ import { ProductSlice, productSlice } from "./productSlice";
 import { WilayaSlice, uiSlice } from "./wilayaSlice";
 import { DashboardSlice, dashboardSlice } from "./dashboardSlice";
 
+import { devtools } from "zustand/middleware";
+
 export const useStore = create<
   ProductSlice & WilayaSlice & OrderFormSlice & SnackbarSlice & DashboardSlice
->()((...args) => ({
-  ...productSlice(...args),
-  ...uiSlice(...args),
-  ...orderFormSlice(...args),
-  ...snackbarSlice(...args),
-  ...dashboardSlice(...args)
-}));
+>()(
+  devtools(
+    (...args) => ({
+      ...productSlice(...args),
+      ...uiSlice(...args),
+      ...orderFormSlice(...args),
+      ...snackbarSlice(...args),
+      ...dashboardSlice(...args),
+    }),
+    {
+      // enabled: process.env.NODE_ENV === "development",
+      name: "zustandStore",
+    }
+  )
+);

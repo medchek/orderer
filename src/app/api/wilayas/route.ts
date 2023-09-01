377c4dc5c46179148fe9d89f6599ea0b29258d3b
medchek <wizzsha@gmail.com>
2023-09-01 21:35:56 +0100

    feat: remove admin check for wialyas get request
    Since both parties now (admin and non admin users) receive the same data
    there is no longer the need to differentiate between the two

--- src/app/api/wilayas/route.ts
+++ src/app/api/wilayas/route.ts
@@ -1,40 +1,25 @@
-import { apiErrorResponse } from './../../../lib/utils';
+import { apiErrorResponse } from "./../../../lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/db";
 import { cache } from "@/lib/cache";
 import { isAdmin as isAdministrator } from "../auth/[...nextauth]/route";
-import { STATUS_BAD_REQUEST, STATUS_OK, STATUS_UNAUTHORIZED } from "@/lib/constants";
-import { PatchShippingPricesRequestPayload } from '@/types/api';
-import Joi from 'joi';
+import {
+  STATUS_BAD_REQUEST,
+  STATUS_OK,
+  STATUS_UNAUTHORIZED,
+} from "@/lib/constants";
+
+import Joi from "joi";
+import { PatchShippingPricesRequestPayload } from "@/features/shipping-prices/api/patchWilaya";
 // TODO: Implement Rate limiting for all API routes
 
 export async function GET() {
-
-  const isAdmin = await isAdministrator()
-
   try {
     // get cached results
     const cachedWilayas = cache.get("wilayas");
     const ttl =
       process.env.NODE_ENV === "production" ? 120 * (60 * 1000) : 1000 * 60;
-    // include the availability status when sending it to the admin
-    if (isAdmin) {
-      const wilayasForAdmin = await prisma.wilaya.findMany({
-        select: {
-          name: true,
-          code: true,
-          homePrice: true,
-          officePrice: true,
-          arName: true,
-          availableHome: true,
-          availableOffice: true,
-        },
-        orderBy: {
-          code: "asc"
-        }
-      });
-      return NextResponse.json(wilayasForAdmin, { status: STATUS_OK });
-    }
+
     // For regular users
     // TODO: RATE LIMIT
     if (cachedWilayas === undefined) {
@@ -50,8 +35,8 @@ export async function GET() {
           availableHome: true,
         },
         orderBy: {
-          code: "asc"
-        }
+          code: "asc",
+        },
       });
       // then save the data in the cache
       cache.set("wilayas", wilayas, { ttl });
@@ -70,32 +55,37 @@ export async function GET() {
 export async function PATCH(req: NextRequest) {
   // TODO: Delete cache when updated
 
-  const isAdmin = await isAdministrator()
+  const isAdmin = await isAdministrator();
   if (!isAdmin) {
-    return apiErrorResponse("unauthorized", STATUS_UNAUTHORIZED)
+    return apiErrorResponse("unauthorized", STATUS_UNAUTHORIZED);
   }
   try {
     const body: PatchShippingPricesRequestPayload = await req.json();
 
     const schema = Joi.object<PatchShippingPricesRequestPayload>({
-      wilayas: Joi.array().min(1).max(58).items(Joi.number().strict().required().min(1).max(58)).required(),
+      wilayas: Joi.array()
+        .min(1)
+        .max(58)
+        .items(Joi.number().strict().required().min(1).max(58))
+        .required(),
       homePrice: Joi.number().strict().min(0).positive().precision(2),
       officePrice: Joi.number().strict().min(0).positive().precision(2),
       availableHome: Joi.boolean().strict(),
-      availableOffice: Joi.boolean().strict()
-    })
+      availableOffice: Joi.boolean().strict(),
+    });
 
     const data = schema.validate(body);
 
     if (!data.value || data.error) {
-      console.error("Error validating patch wilaya data", data.error)
+      console.error("Error validating patch wilaya data", data.error);
       return apiErrorResponse("Invalid request", STATUS_BAD_REQUEST);
     }
 
-    const { wilayas, availableHome, availableOffice, homePrice, officePrice } = data.value
-    console.log("updating wilya with values", data.value)
+    const { wilayas, availableHome, availableOffice, homePrice, officePrice } =
+      data.value;
+    console.log("updating wilya with values", data.value);
     /**
-     * todo: before updateing, and if the user wants to make a wilaya unavailable, 
+     * todo: before updateing, and if the user wants to make a wilaya unavailable,
      * todo: check if at least one wilaya wil still be available for shipping
      */
     // if the user requests to make a wilaya unavailable
@@ -106,7 +96,7 @@ export async function PATCH(req: NextRequest) {
     await prisma.wilaya.updateMany({
       where: {
         code: {
-          in: wilayas
+          in: wilayas,
         },
       },
       data: {
@@ -114,17 +104,13 @@ export async function PATCH(req: NextRequest) {
         availableOffice,
         homePrice,
         officePrice,
-      }
-    })
+      },
+    });
 
-
-    console.log("successfully updated wilaya")
     // delete cache when request is valid
     cache.delete("wilayas");
-    return NextResponse.json("ok", { status: STATUS_OK })
+    return NextResponse.json("ok", { status: STATUS_OK });
   } catch (error) {
-    return apiErrorResponse("Error updating wilaya shipping prices")
-
+    return apiErrorResponse("Error updating wilaya shipping prices");
   }
-
 }

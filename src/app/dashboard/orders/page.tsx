1115e65c3ae0da27727e643ed245dbd453ac7cec
medchek <wizzsha@gmail.com>
2023-09-04 20:27:29 +0100

    feat: update orders dashboard page component

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -2,161 +2,172 @@
 
 import ModalLoader from "@/components/ModalLoader";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
-import DashboardSearchInput from "@/components/dashboard/DashboardSearchInput";
-import { deleteOrder, getOrders } from "@/lib/clientApiHelpers";
-import {
-  addPartitive,
-  discountedPrice,
-  formatDate,
-  trucateString,
-} from "@/lib/utils";
+
 import dynamic from "next/dynamic";
-import { useState } from "react";
-import { MdDeleteOutline, MdOutlineSettings } from "react-icons/md";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import { BsListNested } from "react-icons/bs";
 import { useStore } from "@/store";
-import { createPortal } from "react-dom";
+import DashboardOrdersCard from "@/features/orders/components/DashboardOrdersCard";
+import DashboardFetchError from "@/components/dashboard/DashboardFetchError";
+import DashboardPagination from "@/components/dashboard/DashboardPagination";
+
+import DashboardEmptyState from "@/components/dashboard/DashboardEmptyState";
+import DashboardOrdersBlockUser from "@/features/orders/components/DashboardOrdersBlockUser";
+import DashboardOrdersToolbar from "@/features/orders/components/DashboardOrdersToolbar";
+import { useFetchOrders } from "@/features/orders/api/getOrders";
+import { useDeleteOrder } from "@/features/orders/api/deleteOrder";
+import { usePatchOrder } from "@/features/orders/api/patchOrder";
+import { queryKeys } from "@/lib/queryKeys";
+
 
 const DashboardDeleteConfirm = dynamic(
   () => import("@/components/dashboard/DashboardDeleteConfirm"),
   { loading: () => <ModalLoader /> }
 );
 
-type Props = {};
-
-export default function Orders({}: Props) {
+export default function Orders() {
   const queryClient = useQueryClient();
-  const { showSnackbar } = useStore();
-  const [hasFetched, setHasFetched] = useState(false);
+  const {
+    showSnackbar,
+    orderPhoneToBlock,
+    setOrderCodeToDelete,
+    orderCodeToDelete,
+    setOrderPhoneToBlock,
+    ordersQueryFilters,
+    ordersQueryFilters: { currentPage },
+    setOrdersCurrentPage: setCurrentPage,
+  } = useStore();
 
-  const portalTargetElementId = "dashboard-orders-table-errors-display";
-
-  const { isFetching, data: ordersData } = useQuery({
-    queryKey: ["orders"],
-    queryFn: getOrders,
-    keepPreviousData: true,
-    retryDelay: 2000,
-    cacheTime: 1000 * 60 * 10,
-    onSuccess: () => {
-      setHasFetched(true);
-    },
-    enabled: !hasFetched,
+  const {
+    isFetching,
+    isError: isFetchingOrderError,
+    data: ordersData,
+    refetch,
+  } = useFetchOrders({
+    filters: ordersQueryFilters,
   });
 
-  const [orderCodeToDelete, setOrderCodeToDelete] = useState<string | null>(
-    null
-  );
-  const { mutate: deleteMutation, isLoading: isDeleting } = useMutation({
-    mutationKey: ["deleteOrder"],
-    mutationFn: deleteOrder,
-    onSuccess(deletedOrderCode) {
+  // pagination
+  const ordersPerPage = 10;
+  const pageCount = Math.ceil((ordersData?.count ?? 0) / ordersPerPage);
+
+  const handlePageChange = (event: { selected: number }) => {
+    const selected = event.selected;
+    if (selected === currentPage) return;
+    setCurrentPage(selected);
+  };
+
+  const { mutate: deleteMutation, isLoading: isDeleting } = useDeleteOrder({
+    onSuccess: (deletedOrderCode) => {
       if (!ordersData) return;
-      const ordersDataCopy = ordersData.filter(
+      const ordersDataCopy = ordersData.data.filter(
         (order) => order.code !== deletedOrderCode
       );
-      queryClient.setQueryData(["orders"], ordersDataCopy);
+      queryClient.setQueryData(
+        queryKeys.orders.all(ordersQueryFilters).queryKey,
+        {
+          count: ordersData.count,
+          data: ordersDataCopy,
+        }
+      );
+      // queryClient.invalidateQueries({queryKey: ["orders"]})
+      showSnackbar("Commande supprimée", "default");
       // close the modal
       setOrderCodeToDelete(null);
-      showSnackbar("Commande supprimée!", "default");
+    },
+    onError: () => {
+      showSnackbar("Une erreur est survenu, veuillez reéssayer", "error");
     },
   });
 
-  const displayData = () => {
+  const {
+    mutateAsync: patchMutation,
+    isLoading: isPatching,
+    variables: patchData,
+  } = usePatchOrder({
+    onSuccess() {
+      // close modal
+      setOrderPhoneToBlock(null);
+      queryClient.invalidateQueries({ queryKey: queryKeys.orders._def });
+      showSnackbar("Commande Modifiée", "default");
+    },
+    onError: () => {
+      showSnackbar("Une erreur est survenu, veuillez reéssayer", "error");
+    },
+  });
+
+  const displayOrdersData = () => {
     if (isFetching) {
       // SKELETON LOADERS
-      return Array.from({ length: 11 }, (_, i) => (
-        <tr key={`tr-${i}`} className="h-16 animate-pulse">
-          {Array.from({ length: 9 }, (_, idx) => (
-            <td key={`td-${idx}`}>
-              <div className="bg-stone-800 h-6 w-4/5 rounded-lg"></div>
-            </td>
-          ))}
-        </tr>
+      return Array.from({ length: 8 }, (_, i) => (
+        <div
+          key={`tr-${i}`}
+          className="h-auto rounded-md animate-pulse bg-stone-900/80 py-3 px-4 [&>section>div]:bg-stone-800 [&>section>div]:rounded-md space-y-2"
+        >
+          <section className="flex gap-2 [&>div]:h-6 ">
+            <div className="w-1/6"></div>
+            <div className="h-6  w-1/12"></div>
+            <div className="h-6  w-20"></div>
+          </section>
+          <section className="flex gap-2 [&>div]:h-5">
+            <div className="w-24"></div>
+            <div className="w-20"></div>
+            <div className="w-72"></div>
+            <div className="w-32"></div>
+          </section>
+          <hr className="border-stone-800" />
+          <section className="flex justify-between">
+            <div className="w-72 h-6"></div>
+            <section className="flex flex-col items-end gap-2">
+              <div className="w-40 rounded-md bg-stone-800 h-4"></div>
+              <div className="w-44 rounded-md bg-stone-800 h-6"></div>
+            </section>
+          </section>
+        </div>
       ));
+    } else if (isFetchingOrderError) {
+      return (
+        <DashboardFetchError
+          refetch={refetch}
+          text="Une érreur s'est produite lors de la recherche des commandes"
+          className="relative flex  h-full justify-center"
+        />
+      );
     } else {
       // no data
-      if (!ordersData || ordersData.length === 0) {
-        return createPortal(
-          <div className="w-96 h-auto -translate-y-12">
-            <div className="w-96 text-center flex flex-col items-center gap-1  text-stone-100">
-              <BsListNested className="w-20 h-20" />
-              <p>Aucune commande n'a été encore enregistrée</p>
-              <p className="text-stone-400 text-sm">
-                Vous serez informé via notifications chaque fois qu'une nouvelle
-                commande est enregistrée
-              </p>
-            </div>
-          </div>,
-          document.getElementById(portalTargetElementId) as Element
+      if (!ordersData || ordersData.data.length === 0) {
+        const hasFilter = !!Object.entries(ordersQueryFilters).filter(
+          ([_, value,]) => !!value
+        ).length;
+        return (
+          <DashboardEmptyState
+            Icon={<BsListNested className="w-20 h-20" />}
+            text={
+              hasFilter
+                ? "Commandes non trouvées"
+                : "Aucune commande n'a été encore enregistrée"
+            }
+            subContent={
+              hasFilter
+                ? "Aucune commande ne corresponds à la recherche ou au filtres séléctionés."
+                : "Vous serez informé via notifications chaque fois qu'une nouvelle commande est enregistrée"
+            }
+          />
         );
       }
       // DATA Display
       else
-        return ordersData?.map((order) => {
-          const {
-            address,
-            code,
-            createdAt,
-            isHome,
-            orderProducts,
-            status,
-            user,
-            wilaya,
-          } = order;
-          const totalPrice = () => {
-            const productsPrice = orderProducts.reduce((prev, { product }) => {
-              return prev + discountedPrice(product.price, product.discount);
-            }, 0);
-
-            const shippingPrice = isHome
-              ? wilaya.homePrice
-              : wilaya.officePrice;
-
-            return productsPrice + shippingPrice;
-          };
+        return ordersData.data.map((order) => {
+          const { code } = order;
 
           return (
-            <tr
+            <DashboardOrdersCard
               key={code}
-              className="h-28 max-h-[7rem] [&>td]:pr-4 [&>td]:pt-4 [&>td]:align-top overflow-hidden"
-            >
-              <td className="text-stone pl-4" title={code}>
-                {trucateString(code, 6)}
-              </td>
-              <td className="font-semibold">{user.phone}</td>
-              <td>{wilaya.name}</td>
-              <td>
-                {isHome
-                  ? address
-                  : `Bureau de livraison de la wilaya ${addPartitive(
-                      wilaya.name
-                    )}`}
-              </td>
-              <td className="[&>p]:line-clamp-1">
-                {orderProducts.map(({ product }, i) => (
-                  <p key={i}>{product.name}</p>
-                ))}
-              </td>
-              <td>{totalPrice()}DA</td>
-              <td>
-                <p className="flex w-fullitems-center justify-center rounded-lg bg-stone-950 p-1 text-blue-600">
-                  {status}
-                </p>
-              </td>
-              <td>{formatDate(createdAt)}</td>
-              <td className="flex gap-2">
-                <button className="h-8 w-8 rounded-lg transition-colors dark:hover:bg-stone-800 dark:focus:bg-stone-900">
-                  <MdOutlineSettings className="h-6 w-6" />
-                </button>
-                <button
-                  className="h-8 w-8 rounded-lg transition-colors dark:hover:bg-stone-800 dark:focus:bg-stone-900"
-                  onClick={() => setOrderCodeToDelete(code)}
-                >
-                  <MdDeleteOutline className="h-6 w-6" />
-                </button>
-              </td>
-            </tr>
+              updateStatus={patchMutation}
+              isPatching={isPatching}
+              patchingCode={patchData?.code}
+              {...order}
+            />
           );
         });
     }
@@ -165,48 +176,28 @@ export default function Orders({}: Props) {
   return (
     <div
       id="dashbarod-orders"
-      className="flex w-full grow flex-col justify-between overflow-y-hidden px-6 pt-3"
+      className="flex w-full grow flex-col justify-between overflow-y-hidden pt-3"
     >
-      <section className="flex grow flex-col overflow-hidden">
+      <section className="flex grow flex-col overflow-hidden px-6">
         <DashboardHeader label="Commandes" noPadding />
+        <DashboardOrdersToolbar />
+
         <div
-          id="orders-toolbar"
-          className="flex h-16 min-h-[4rem] w-full items-center justify-end "
+          id="orders-list"
+          className="flex flex-col gap-4 overflow-y-auto w-full dark:[color-scheme:dark] grow"
         >
-          <DashboardSearchInput placeholder="Cherche une commande" />
+          {displayOrdersData()}
         </div>
-        <div
-          id="table-container"
-          className="flex overflow-auto w-full dark:[color-scheme:dark]"
-        >
-          <table className="table-fixed text-left text-sm w-full">
-            <thead>
-              <tr className="h-14 sticky top-0 z-10 dark:bg-dark  text-stone-500">
-                <th className="w-28 pl-4">#</th>
-                <th className="w-36">Telephone</th>
-                <th className="w-28">Wilaya</th>
-                <th className="w-64">Adresse</th>
-                <th className="w-64 2xl:w-72">Produits</th>
-                <th className="w-28">Prix Total</th>
-                <th className="w-32">Statut</th>
-                <th className="w-44">Date</th>
-                <th className="w-auto">Actions</th>
-              </tr>
-            </thead>
-            <tbody className="relative text-stone-50">{displayData()}</tbody>
-          </table>
-        </div>
-        <div
-          id={portalTargetElementId}
-          className="grow w-full  flex justify-center items-center"
-        ></div>
       </section>
-      <section
+      {/* <section
         id="pagination"
         className="h-10 min-h-[2.5rem] bg-blue-400 w-full"
-      >
-        test
-      </section>
+      > */}
+      <DashboardPagination
+        pageCount={pageCount}
+        onPageChange={handlePageChange}
+      />
+      {/* </section> */}
       {orderCodeToDelete && (
         <DashboardDeleteConfirm
           label="Supprimer une commande"
@@ -216,6 +207,19 @@ export default function Orders({}: Props) {
           onConfirm={() => deleteMutation(orderCodeToDelete)}
         />
       )}
+      {orderPhoneToBlock && (
+        <DashboardOrdersBlockUser
+          phone={orderPhoneToBlock}
+          closeModal={() => {
+            setOrderPhoneToBlock(null);
+          }}
+        />
+      )}
     </div>
   );
 }
+
+/**
+ * TODO: Search by phone, id, wilaya, status, created
+ *
+ */

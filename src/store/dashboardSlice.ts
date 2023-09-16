d1810baa7a016b600f45b316f31beb3f0f0e0b0d
medchek <wizzsha@gmail.com>
2023-09-16 11:22:39 +0100

    feat: add dashboard state to store

--- src/store/dashboardSlice.ts
+++ src/store/dashboardSlice.ts
@@ -1,3 +1,4 @@
+import { GetOrdersQueryFilter } from "@/features/orders/api/getOrders";
 import { StateCreator } from "zustand";
 
 export type CategoryType = "category" | "subcategory";
@@ -13,10 +14,13 @@ export interface CategoryDataOpen {
 export interface DashboardSlice {
   // products
   isAddProductOpen: boolean;
-  setIsAddProductOpen: (v: boolean) => void
+  setIsAddProductOpen: (v: boolean) => void;
+  // image
+  isUploadingImage: boolean;
+  setIsUploadingImage: (v: boolean) => void;
 
   // Categories
-  deleteCategoryData: CategoryDataClosed | CategoryDataOpen
+  deleteCategoryData: CategoryDataClosed | CategoryDataOpen;
   setDeleteCategoryData: (v: CategoryDataClosed | CategoryDataOpen) => void;
   editCategoryData: CategoryDataClosed | CategoryDataOpen;
   setEditCategoryData: (v: CategoryDataClosed | CategoryDataOpen) => void;
@@ -28,42 +32,81 @@ export interface DashboardSlice {
   categoryFilterTerm: string;
   categoryFilterType: CategoryType;
 
-  setCategoryFilterType: (v: CategoryType) => void
+  setCategoryFilterType: (v: CategoryType) => void;
   /** The provided value will be trimmed and lowercased */
-  setCategoryFilterTerm: (v: string) => void
+  setCategoryFilterTerm: (v: string) => void;
 
+  // orders
+  orderCodeToDelete: string | null;
+  setOrderCodeToDelete: (code: string | null) => void;
+  ordersQueryFilters: GetOrdersQueryFilter;
+  setOrdersQueryFilters: (filter: GetOrdersQueryFilter) => void;
 
+  setOrdersCurrentPage: (p: number) => void;
+
+  orderPhoneToBlock: string | null;
+  setOrderPhoneToBlock: (phone: string | null) => void;
+
+  // blacklist
+
+  blacklistedNumberIdToDelete: number | null;
+  setBlacklistedNumberIdToDelete: (n: number | null) => void;
 }
 
 export const dashboardSlice: StateCreator<DashboardSlice> = (set) => ({
   // Products
   isAddProductOpen: false,
   setIsAddProductOpen: (v: boolean) => {
-    set(() => ({ isAddProductOpen: v }))
+    set(() => ({ isAddProductOpen: v }));
+  },
+  isUploadingImage: false,
+  setIsUploadingImage: (v) => {
+    set(() => ({ isUploadingImage: v }));
   },
 
   // Categories
   deleteCategoryData: { isOpen: false },
   setDeleteCategoryData: (data) => {
-    set(() => ({ deleteCategoryData: data }))
+    set(() => ({ deleteCategoryData: data }));
   },
   editCategoryData: { isOpen: false },
   setEditCategoryData: (data) => {
-    set(() => ({ editCategoryData: data }))
+    set(() => ({ editCategoryData: data }));
   },
-  categoryPerPage: 5,
+  categoryPerPage: 10,
   setCategoryPerPage: (n) => {
-    set(() => ({ categoryPerPage: n }))
+    set(() => ({ categoryPerPage: n }));
   },
   categoryFilterTerm: "",
   categoryFilterType: "category",
   setCategoryFilterTerm: (v) => {
-    set(() => ({ categoryFilterTerm: v.trim().toLowerCase() }))
+    set(() => ({ categoryFilterTerm: v.trim().toLowerCase() }));
   },
   setCategoryFilterType: (v) => {
-    set(() => ({ categoryFilterType: v, categoryFilterTerm: "" }))
+    set(() => ({ categoryFilterType: v, categoryFilterTerm: "" }));
+  },
+  orderCodeToDelete: null,
+  setOrderCodeToDelete: (code) => set(() => ({ orderCodeToDelete: code })),
+  ordersQueryFilters: { currentPage: 0 },
+  setOrdersQueryFilters: (filter: GetOrdersQueryFilter) => {
+    set(() => ({ ordersQueryFilters: filter }));
+  },
+  setOrdersCurrentPage: (page) => {
+    set((state) => {
+      return {
+        ordersQueryFilters: {
+          ...state.ordersQueryFilters,
+          currentPage: page,
+        },
+      };
+    });
+  },
 
-  }
+  orderPhoneToBlock: null,
+  setOrderPhoneToBlock: (phone) => set(() => ({ orderPhoneToBlock: phone })),
 
-
+  blacklistedNumberIdToDelete: null,
+  setBlacklistedNumberIdToDelete: (n) => {
+    set(() => ({ blacklistedNumberIdToDelete: n }));
+  },
 });

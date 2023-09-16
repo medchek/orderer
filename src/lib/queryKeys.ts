3caff49cb463ff5f9b9306666c88d9daf2ded9c8
medchek <wizzsha@gmail.com>
2023-09-16 11:23:09 +0100

    feat: add query keys factory

--- src/lib/queryKeys.ts
+++ src/lib/queryKeys.ts
@@ -1,36 +1,31 @@
-import { Status } from "@prisma/client";
+import { GetOrdersQueryFilter } from "@/features/orders/api/getOrders";
+import { createQueryKeyStore } from "@lukemorales/query-key-factory";
 
-export type GetOrdersQueryFilter = {
-  status?: Status | "";
-  wilaya?: string;
-  shippingType?: string;
-  phone?: string;
-  code?: string;
-};
-
-/**
- * Query key for fetching the orders
- * @param currentPage the current pagination page
- * @param queryFilter any filters to the query
- * @returns query key array
- */
-export const fetchOrdersQueryKey = (
-  currentPage: number,
-  queryFilter: GetOrdersQueryFilter
-): [string, number, GetOrdersQueryFilter] => [
-  "orders",
-  currentPage,
-  queryFilter,
-];
-
-/**
- * Query key for fetching all the wilayas
- * @returns query key array
- */
-export const fetchWilayaQueryKey = ["wilayas"] as const;
-
-
-/**
- * Query key for patch wilaya shipping prices and shipping availablity mutation
- */
-export const patchWilayaMutationKey = ["wilayas", "patch"] as const;
+export const queryKeys = createQueryKeyStore({
+  orders: {
+    all: (filters: GetOrdersQueryFilter) => ({
+      queryKey: [{ filters }],
+    }),
+  },
+  wilayas: {
+    all: null,
+  },
+  towns: {
+    list: (wilayaCode: number) => ({ queryKey: [wilayaCode] }),
+  },
+  blacklist: {
+    all: null,
+  },
+  categories: {
+    all: null,
+  },
+  products: {
+    all: null,
+    one: (code: string) => ({
+      queryKey: [code],
+    }),
+  },
+  images: {
+    all: null,
+  },
+});

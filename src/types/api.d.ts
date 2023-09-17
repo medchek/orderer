f89c148c432a8d3e78ad5378092ca034555f8ef3
medchek <wizzsha@gmail.com>
2023-09-17 19:01:01 +0100

    refactor: move types to features folder

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,141 +1,24 @@
-import { OrderStatus } from "@/lib/constants";
-import { Product } from "@/store/productSlice";
-import { Status } from "@prisma/client";
+import {
+  UseMutationOptions,
+  UseQueryOptions
+} from "@tanstack/react-query";
+import { HTTPError } from "ky";
 
+export type PromiseStatus = "init" | "fetching" | "success" | "error";
 
-export type PromiseStatus = "init" | "fetching" | "success" | "error"
+export type MutationOptions<
+  /** Returned data type  */
+  TData = unknown,
+  /** Payload type */
+  TVariables = void,
+  TContext = unknown,
+> = Omit<
+  UseMutationOptions<TData, HTTPError, TVariables, TContext>,
+  "mutationFn"
+>;
 
+export type QueryOptions<TReturnData = unknown> = Omit<
+  UseQueryOptions<TReturnData, HTTPError, TReturnData>,
+  "queryFn"
+>;
 
-/**
- * Successful response payload for api/images/ POST request
- */
-export interface PostImageSuccessResponsePayload {
-  id: string;
-  originalName: string;
-  originalSize: number;
-}
-export interface GetProductsSuccessResponsePayload {
-  count: number;
-  products: Product[]
-}
-export interface PostProductBodyPayload {
-  name: string;
-  price: number;
-  description: string;
-  stock: number | null;
-  category: { categoryId: number, subcategoryId?: number } | null;
-  discount: number;
-  images: string[];
-}
-
-
-
-export interface PostProductSuccessResponsePayload extends Product { }
-
-export interface PatchProductBodyPayload {
-  name?: string;
-  price?: number;
-  description?: string;
-  stock?: number | null;
-  categoryId?: number | null;
-  discount?: number;
-  images?: {
-    added: string[];
-    removed: string[];
-  };
-}
-export interface PatchProductSuccessResponsePayload extends PostProductSuccessResponsePayload { }
-
-export interface PatchShippingPricesRequestPayload {
-  homePrice?: number;
-  officePrice?: number;
-  availableHome?: boolean;
-  availableOffice?: boolean;
-  wilayas: number[];
-}
-
-export interface PostOrderRequestPayload {
-  phone: string;
-  name?: string;
-  lastName?: string;
-  email?: string;
-  address?: string;
-  isHome: boolean;
-  wilayaId: number;
-  townCode: number;
-  productsCode: string[];
-}
-export interface PostOrderSuccessResponsePayload {
-  orderCode: string;
-}
-
-export interface GetAllOrdersSuccessResponsePayload {
-  address: string | null;
-  code: string;
-  isHome: boolean;
-  status: Status;
-  createdAt: Date;
-  user: {
-    phone: string | null;
-  };
-  wilaya: {
-    name: string;
-    arName: string;
-    code: number;
-    homePrice: number;
-    officePrice: number;
-  };
-  town: {
-    code: number,
-    arName: string,
-    name: string
-  }
-  orderProducts: {
-    product: {
-      name: string;
-      price: number;
-      discount: number;
-    };
-  }[]
-}
-
-export interface PostCategoryRequestPayload {
-  name: string;
-}
-export interface PostCategorySuccessReponsePayload {
-  id: number;
-  name: string;
-}
-
-export type GetCategoriesSuccessResponsePayload = {
-  id: number;
-  name: string;
-  subCategories?: { id: number; name: string }[]
-}[]
-
-export interface PostSubCategoryRequestPayload {
-  categoryId: number;
-  name: string;
-}
-
-export interface PostSubCategorySuccessResponsePayload extends PostSubCategoryRequestPayload {
-  id: number
-}
-
-export type GetTownsSuccessResponsePayload = {
-  name: string;
-  arName: string;
-  code: number;
-}[]
-
-export interface PatchCategoryRequestPayload {
-  name: string;
-}
-export interface PatchSubcategorySuccessResponsePayload {
-  id: number;
-  /** Parent category id */
-  categoryId: number;
-  name: string;
-}
-
-export interface PatchCategorySuccessResponsePayload extends PatchCategoryRequestPayload { id: number; }
--- src/types/components.d.ts
+++
@@ -1,8 +0,0 @@
-export type UploadStatus = "error" | "uploading" | "success";
-
-export interface FileMetaData {
-  imageId: string | null;
-  file: File;
-  status: UploadStatus;
-  url: string;
-}

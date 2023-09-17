1f57fa7f8ca1c471ad97de70b4998612d1184133
medchek <wizzsha@gmail.com>
2023-09-17 19:22:10 +0100

    feat: categories can be set to products

--- src/app/api/products/[code]/route.ts
+++ src/app/api/products/[code]/route.ts
@@ -7,14 +7,17 @@ import {
   STATUS_OK,
   STATUS_UNAUTHORIZED,
 } from "@/lib/constants";
-import { apiErrorResponse, } from "@/lib/utils";
+import { apiErrorResponse } from "@/lib/utils";
 import Joi from "joi";
-import { PatchProductBodyPayload } from "@/types/api";
 import { isAdmin } from "../../auth/[...nextauth]/route";
+import {
+  PatchProductBodyPayload,
+  PatchProductSuccessResponse,
+} from "@/features/products/api/patchProduct";
 
 export async function GET(
   _: NextRequest,
-  { params }: { params: { code: string } }
+  { params }: { params: { code: string } },
 ) {
   try {
     if (!params.code || params.code.length !== PRODUCT_CODE_LENGTH) {
@@ -53,14 +56,14 @@ export async function GET(
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { code: string } }
+  { params }: { params: { code: string } },
 ) {
   if (!params.code || params.code.length !== PRODUCT_CODE_LENGTH) {
     return NextResponse.json("invalid param", { status: STATUS_BAD_REQUEST });
   }
 
   try {
-    if (!await isAdmin()) {
+    if (!(await isAdmin())) {
       return NextResponse.json("unauthorized", { status: STATUS_UNAUTHORIZED });
     }
 
@@ -68,11 +71,28 @@ export async function PATCH(
 
     const schema = Joi.object<PatchProductBodyPayload>({
       name: Joi.string().min(2).max(150),
-      price: Joi.number().strict().positive().min(1).precision(2).max(1000000000000),
+      price: Joi.number()
+        .strict()
+        .positive()
+        .min(1)
+        .precision(2)
+        .max(1000000000000),
       description: Joi.string().allow(""),
+      category: Joi.object({
+        categoryId: Joi.number().strict().precision(0).positive().required(),
+        subcategoryId: Joi.number().strict().precision(0).positive().optional(),
+      })
+        .strict()
+        .allow(null)
+        .optional(),
       stock: Joi.number().strict().positive().precision(0).min(1).allow(null),
-      categoryId: Joi.number().strict().positive().precision(0).allow(null),
-      discount: Joi.number().strict().positive().precision(0).allow(0).min(0).max(100),
+      discount: Joi.number()
+        .strict()
+        .positive()
+        .precision(0)
+        .allow(0)
+        .min(0)
+        .max(100),
       images: Joi.object({
         added: Joi.array()
           .min(0)
@@ -89,7 +109,7 @@ export async function PATCH(
 
     if (validation.error || !validation.value) {
       console.log("validationError:", validation.error);
-      return apiErrorResponse("Invalid request", STATUS_BAD_REQUEST)
+      return apiErrorResponse("Invalid request", STATUS_BAD_REQUEST);
     }
 
     const data = validation.value;
@@ -99,30 +119,30 @@ export async function PATCH(
       where: { code: params.code },
       include: {
         images: true,
-      }
-    })
+      },
+    });
 
     if (!product) {
-      return apiErrorResponse("product not found", STATUS_NOT_FOUND)
+      return apiErrorResponse("product not found", STATUS_NOT_FOUND);
     }
 
-    const dbimages = product.images.map((data => data.id))
+    const dbimages = product.images.map((data) => data.id);
 
     // if there are images to remove
     if (data.images && data.images.removed.length > 0) {
       // delete them from the db only if they belong to the request product
       const validImgIds = data.images.removed.every((imgId) => {
-        return dbimages.includes(imgId)
-      })
+        return dbimages.includes(imgId);
+      });
       // if the images are valid, delete them from the db
       if (validImgIds) {
         await prisma.image.deleteMany({
           where: {
             id: {
-              in: data.images.removed
-            }
-          }
-        })
+              in: data.images.removed,
+            },
+          },
+        });
       }
     }
     // check if the images to add exist in the db
@@ -130,53 +150,82 @@ export async function PATCH(
       const addImgDbCount = await prisma.image.count({
         where: {
           id: {
-            in: data.images.added
-          }
-        }
-      })
+            in: data.images.added,
+          },
+        },
+      });
       // if the count is the same
       if (addImgDbCount !== data.images.added.length) {
-        return apiErrorResponse("Invalid request. Add images wrong id", STATUS_BAD_REQUEST)
+        return apiErrorResponse(
+          "Invalid request. Add images wrong id",
+          STATUS_BAD_REQUEST,
+        );
       }
     }
 
-
-    const { images, ...rest } = data;
-    const updatedProduct = await prisma.product.update({
+    const { images, category, ...rest } = data;
+    const patchedProduct = await prisma.product.update({
       where: {
         code: params.code,
       },
       data: {
         ...rest,
-        ...(images?.added.length && {
-          images: {
-            connect: images.added.map((id) => ({ id }))
-          }
-        })
-      },
-      include: {
-        images: true
-      },
+        images: {
+          connect: images?.added?.map((id) => ({ id })),
+          disconnect: images?.removed?.map((id) => ({ id })),
+        },
 
-    })
-    const { id, ...responsePayload } = updatedProduct;
-    return NextResponse.json(responsePayload, { status: STATUS_OK })
+        categoryId: category ? category.categoryId : category,
+        subCategoryId:
+          category && category.subcategoryId
+            ? category?.subcategoryId
+            : null,
+      },
+      select: {
+        category: {
+          select: {
+            name: true,
+          },
+        },
+        subCategory: {
+          select: {
+            name: true,
+          },
+        },
+        code: true,
+        description: true,
+        discount: true,
+        images: {
+          select: {
+            id: true,
+          },
+        },
+        name: true,
+        price: true,
+        stock: true,
+      },
+    });
 
+    return NextResponse.json<PatchProductSuccessResponse>(patchedProduct, {
+      status: STATUS_OK,
+    });
   } catch (error) {
-    return apiErrorResponse("Error updating product")
+    console.error("Erroring Patching product =>", error);
+
+    return apiErrorResponse("Error updating product");
   }
 }
 
 export async function DELETE(
   _: NextRequest,
-  { params }: { params: { code: string } }
+  { params }: { params: { code: string } },
 ) {
   if (!params.code || params.code.length !== PRODUCT_CODE_LENGTH) {
     return apiErrorResponse("Invalid param", STATUS_BAD_REQUEST);
   }
 
   try {
-    if (!await isAdmin()) {
+    if (!(await isAdmin())) {
       return apiErrorResponse("Unauthorized", STATUS_UNAUTHORIZED);
     }
 
@@ -186,8 +235,6 @@ export async function DELETE(
       },
     });
 
-
-
     console.log("DELETE SUCCESSFUL, RESULTS:", product);
 
     // TODO: REMOVE CASCADED IMAGES FROM THE GOOGLE DRIVE
@@ -195,6 +242,6 @@ export async function DELETE(
     return NextResponse.json("ok", { status: STATUS_OK });
   } catch (error) {
     console.log(`Error deleting product with code [${params.code}]`, error);
-    return apiErrorResponse("Product not found", STATUS_NOT_FOUND)
+    return apiErrorResponse("Product not found", STATUS_NOT_FOUND);
   }
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../../prisma/db";
-import { apiErrorResponse, isUniqueConstraintPrismaError, uniqueId } from "@/lib/utils";
-import Joi from "joi";
 import {
-  GetProductsSuccessResponsePayload,
-  PostProductBodyPayload,
-  PostProductSuccessResponsePayload,
-} from "@/types/api";
+  apiErrorResponse,
+  isUniqueConstraintPrismaError,
+  uniqueId,
+} from "@/lib/utils";
+import Joi from "joi";
+
 import { isAdmin } from "../auth/[...nextauth]/route";
 import {
   PRODUCT_CODE_LENGTH,
@@ -16,10 +16,14 @@ import {
   STATUS_NOT_FOUND,
   STATUS_UNAUTHORIZED,
 } from "@/lib/constants";
+import { GetProductsSuccessResponse } from "@/features/products/api/getProducts";
+import {
+  PostProductBodyPayload,
+  PostProductSuccessResponse,
+} from "@/features/products/api/postProduct";
 
 export async function GET() {
   try {
-
     const count = await prisma.product.count();
 
     const products = await prisma.product.findMany({
@@ -36,11 +40,11 @@ export async function GET() {
           },
         },
         category: {
-          select: { name: true }
+          select: { name: true },
         },
         subCategory: {
-          select: { name: true }
-        }
+          select: { name: true },
+        },
       },
       orderBy: {
         createdAt: "desc",
@@ -48,7 +52,7 @@ export async function GET() {
     });
 
     await prisma.$disconnect();
-    return NextResponse.json<GetProductsSuccessResponsePayload>({ count, products });
+    return NextResponse.json<GetProductsSuccessResponse>({ count, products });
   } catch (e) {
     console.error(e);
     return apiErrorResponse("Error fetching all products");
@@ -57,7 +61,7 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   try {
-    if (!await isAdmin()) {
+    if (!(await isAdmin())) {
       return NextResponse.json("unauthorized", { status: STATUS_UNAUTHORIZED });
     }
     const body: PostProductBodyPayload = await req.json();
@@ -65,14 +69,30 @@ export async function POST(req: NextRequest) {
 
     const schema = Joi.object<PostProductBodyPayload>({
       name: Joi.string().min(2).max(150).required(),
-      price: Joi.number().strict().strict().positive().precision(2).min(1).max(1000000000000).required(),
+      price: Joi.number()
+        .strict()
+        .strict()
+        .positive()
+        .precision(2)
+        .min(1)
+        .max(1000000000000)
+        .required(),
       description: Joi.string().allow(""),
       stock: Joi.number().strict().positive().min(1).precision(0).allow(null),
       category: Joi.object({
-        categoryId: Joi.number().strict().precision(0).required(),
-        subcategoryId: Joi.number().strict().precision(0).optional()
-      }).strict().allow(null).required(),
-      discount: Joi.number().strict().positive().allow(0).min(0).max(100).precision(0),
+        categoryId: Joi.number().strict().precision(0).positive().required(),
+        subcategoryId: Joi.number().strict().precision(0).positive().optional(),
+      })
+        .strict()
+        .allow(null)
+        .required(),
+      discount: Joi.number()
+        .strict()
+        .positive()
+        .allow(0)
+        .min(0)
+        .max(100)
+        .precision(0),
       images: Joi.array()
         .min(1)
         .max(5)
@@ -83,10 +103,10 @@ export async function POST(req: NextRequest) {
     // validate the request json object
     const validation = schema.validate(body);
     if (validation.error || !validation.value) {
-      return apiErrorResponse("Invalid request", STATUS_BAD_REQUEST)
+      return apiErrorResponse("Invalid request", STATUS_BAD_REQUEST);
     }
     // validated data
-    const data = validation.value
+    const data = validation.value;
 
     // check if the posted image ids exist in the db
     const dbImagesIds = await prisma.image.count({
@@ -122,10 +142,12 @@ export async function POST(req: NextRequest) {
       },
     });
 
-    const responsePayload: PostProductSuccessResponsePayload = {
+    const responsePayload: PostProductSuccessResponse = {
       code,
       category: product.category ? { name: product.category.name } : null,
-      subCategory: product.subCategory ? { name: product.subCategory.name } : null,
+      subCategory: product.subCategory
+        ? { name: product.subCategory.name }
+        : null,
       description: product.description,
       discount: product.discount,
       images: product.images,
@@ -134,18 +156,12 @@ export async function POST(req: NextRequest) {
       stock: product.stock,
     };
 
-    return NextResponse.json<PostProductSuccessResponsePayload>(
-      responsePayload,
-      {
-        status: STATUS_CREATED,
-      }
-    );
+    return NextResponse.json<PostProductSuccessResponse>(responsePayload, {
+      status: STATUS_CREATED,
+    });
   } catch (e) {
-
     if (isUniqueConstraintPrismaError(e)) {
-      return apiErrorResponse("product name already exists",
-        STATUS_CONFLICT,
-      );
+      return apiErrorResponse("product name already exists", STATUS_CONFLICT);
     }
 
     console.error("Erros posting a product:", e);

6ea1aec30d8a67f2053dc9542e6174f02b3fab43
medchek <wizzsha@gmail.com>
2023-09-21 22:56:29 +0100

    feat: move dashboard products to a distinct route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,12 @@
 // import AuthProvider from "@/components/AuthProvider";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
-import DashboardProductDisplay from "@/components/dashboard/products/DashboardProductsDisplay";
-import { DashboardProductsToolbar } from "@/components/dashboard/products/DashboardProductsToolbar";
-import React from "react";
 
-type Props = {};
-
-export default async function Dashboard({}: Props) {
+export default async function Dashboard() {
   return (
     <div id="dashboard-products" className="flex grow flex-col py-3 pl-6">
-      <DashboardHeader label="Produits" />
-      <DashboardProductsToolbar />
-      <DashboardProductDisplay />
+      <DashboardHeader label="Acceuil" />
+      {/* <DashboardProductsToolbar />
+      <DashboardProductDisplay /> */}
     </div>
   );
 }
---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,15 @@
+// import AuthProvider from "@/components/AuthProvider";
+import DashboardHeader from "@/components/dashboard/DashboardHeader";
+import DashboardProductsDisplay from "@/features/products/components/dashboard/DashboardProductsDisplay";
+import { DashboardProductsToolbar } from "@/features/products/components/dashboard/DashboardProductsToolbar";
+import React from "react";
+
+export default async function Products() {
+  return (
+    <div id="dashboard-products" className="flex grow flex-col py-3 pl-6">
+      <DashboardHeader label="Produits" />
+      <DashboardProductsToolbar />
+      <DashboardProductsDisplay />
+    </div>
+  );
+}

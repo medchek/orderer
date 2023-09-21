8855f02f66d6d2c6aa4a420398e5bd78b211eb43
medchek <wizzsha@gmail.com>
2023-09-21 23:01:31 +0100

    feat: add order type definitions

---
+++ src/features/orders/types/index.d.ts
@@ -0,0 +1,35 @@
+import { Status } from "@prisma/client";
+
+export interface OrderData {
+    address: string | null;
+    code: string;
+    isHome: boolean;
+    status: Status;
+    createdAt: Date;
+    user: {
+      phone: string | null;
+      blacklist: {
+        phone: string;
+        reason: string | null;
+      } | null;
+    };
+    wilaya: {
+      name: string;
+      arName: string;
+      code: number;
+      homePrice: number;
+      officePrice: number;
+    };
+    town: {
+      code: number;
+      arName: string;
+      name: string;
+    };
+    orderProducts: {
+      product: {
+        name: string;
+        price: number;
+        discount: number;
+      };
+    }[];
+  }

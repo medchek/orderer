0206e6a4b93966793cefff1eb2b2c75f62e477a3
medchek <wizzsha@gmail.com>
2023-09-23 17:14:16 +0100

    fix: modal loader transparency after main component changes

--- src/components/ModalLoader.tsx
+++ src/components/ModalLoader.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import Modal from "./Modal";
 import Loader from "./Loader";
 
-type Props = {};
-
-export default function ModalLoader({}: Props) {
+export default function ModalLoader() {
   return (
     <Modal
       closeModal={() => null}
       hideHeader
       label=""
       className="flex h-full items-center justify-center"
+      transparent
     >
       <Loader className="h-10 w-10" />
     </Modal>

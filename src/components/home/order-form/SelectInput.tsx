0e72c8f1b1313cfd2f50a3f98eccb5094b525780
medchek <wizzsha@gmail.com>
2023-09-27 19:32:20 +0100

    feat: enhance select reusability

--- src/components/home/order-form/SelectInput.tsx
+++ src/components/home/order-form/SelectInput.tsx
@@ -1,18 +1,25 @@
 import Loader from "@/components/Loader";
 import { cn } from "@/lib/utils";
+import clsx from "clsx";
 import React from "react";
 import { RegisterOptions, UseFormRegister } from "react-hook-form";
 import { MdChevronRight } from "react-icons/md";
 
 interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
   isLoading?: boolean;
-  label: string;
+  label?: string;
   name: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   register?: UseFormRegister<any>;
   registerRules?: RegisterOptions;
   children: React.ReactNode;
   error?: string;
   textRight?: string;
+  /** Hides the element dedicated to display error messages that is always present even when no error is displayeds */
+  removeErrorSpace?: boolean;
+  /** Makes the chevron, loader, and text smaller */
+  small?: boolean;
 }
 
 export default function SelectInput({
@@ -26,35 +33,60 @@ export default function SelectInput({
   error,
   disabled,
   textRight,
+  removeErrorSpace,
+  small,
   className,
   ...props
 }: Props) {
   const inputRegister = register && register(name, registerRules);
   return (
     <div className="flex w-full flex-col gap-1">
-      <label htmlFor={id} className="2xl:text-lg font-semibold dark:text-white">
-        {label}
-      </label>
-      <div className="relative flex h-12 w-full items-center">
+      {label?.length && (
+        <label
+          htmlFor={id}
+          className="font-semibold dark:text-white 2xl:text-lg"
+        >
+          {label}
+        </label>
+      )}
+
+      <div className="relative flex w-full items-center">
         <select
           disabled={isLoading || disabled}
+          id={id}
           {...props}
           {...inputRegister}
           className={cn(
-            "h-12 w-full appearance-none rounded-lg bg-[#ECECEC] px-4 placeholder-[#979797] outline-none ring-secondary focus:ring-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-neutral-900 dark:text-white dark:[color-scheme:dark] 2xl:text-base text-sm",
-            className
+            "h-12 w-full appearance-none rounded-lg bg-[#ECECEC] px-4 placeholder-[#979797] outline-none ring-secondary focus:ring-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-neutral-900 dark:text-neutral-300 dark:[color-scheme:dark]",
+            small ? "text-sm" : "text-sm 2xl:text-base",
+            className,
           )}
         >
           {children}
         </select>
 
         {isLoading ? (
-          <Loader className="absolute right-4 h-6 w-6 border-stone-400" />
+          <Loader
+            className={clsx("absolute  border-neutral-500", {
+              "right-1 h-4 w-4": small,
+              "right-4 h-6 w-6": !small,
+            })}
+          />
         ) : (
-          <MdChevronRight className="pointer-events-none absolute right-4 h-7 w-7 rotate-90 dark:text-[#979797]" />
+          <MdChevronRight
+            className={clsx(
+              "pointer-events-none absolute  rotate-90 dark:text-neutral-500",
+              {
+                "right-1 h-5 w-5": small,
+                "right-4 h-7 w-7": !small,
+              },
+            )}
+          />
         )}
       </div>
-      <div className="flex h-5 justify-between ">
+      <div
+        className={`flex h-5 justify-between ${removeErrorSpace && "hidden"}`}
+      >
         <p className="grow text-sm text-red-500">{error && error}</p>
         <p className="grow-0 text-right dark:text-gray-100">{textRight}</p>
       </div>

f86233746da29cd103b69cb5ba19a71f53a7670b
medchek <wizzsha@gmail.com>
2023-10-01 20:25:43 +0100

    feat: add action button to empty state component

--- src/components/dashboard/DashboardEmptyState.tsx
+++ src/components/dashboard/DashboardEmptyState.tsx
@@ -1,23 +1,39 @@
 import { cn } from "@/lib/utils";
 import React from "react";
+import DashboardEmptyStateButton from "./DashboardEmptyStateButton";
 
 interface Props {
   className?: string;
   Icon: React.ReactNode;
   text: string;
   subContent?: React.ReactNode | string;
+  subContentAction?: () => void;
+  /** Display a plus icon before the action button text */
+  actionButtonIcon?: boolean;
 }
 
 export default function DashboardEmptyState({
   Icon,
   subContent,
   text,
+  subContentAction,
+  actionButtonIcon,
   className,
 }: Props) {
   const subContentDisplay = () => {
+    if (subContentAction !== undefined) {
+      return (
+        <DashboardEmptyStateButton
+          onClick={subContentAction}
+          text={subContent}
+          plusIcon={actionButtonIcon}
+        />
+      );
+    }
+
     if (subContent) {
       if (typeof subContent === "string") {
-        return <p className="text-stone-400 text-sm">{subContent}</p>;
+        return <p className="text-sm text-stone-400">{subContent}</p>;
       } else {
         return subContent;
       }
@@ -26,11 +42,11 @@ export default function DashboardEmptyState({
   return (
     <div
       className={cn(
-        "w-full h-full -translate-y-12 flex items-center justify-center",
-        className
+        "flex h-full w-full -translate-y-12 items-center justify-center",
+        className,
       )}
     >
-      <div className="w-96 text-center flex flex-col items-center gap-1  text-stone-100">
+      <div className="flex w-96 flex-col items-center gap-1 text-center  text-stone-100">
         {Icon}
         <p>{text}</p>
 
---
+++ src/components/dashboard/DashboardEmptyStateButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { MdAdd } from "react-icons/md";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: React.ReactNode;
+  /** Display a plus icon before the button text */
+  plusIcon?: boolean;
+}
+
+export default function DashboardEmptyStateButton({
+  text,
+  plusIcon,
+  ...props
+}: Props) {
+  return (
+    <button
+      className="h-10 rounded-lg  px-2 font-semibold transition-colors dark:hover:bg-stone-900 dark:focus:bg-stone-900/70"
+      {...props}
+    >
+      {plusIcon && <MdAdd className="h-7 w-7" />} {text}
+    </button>
+  );
+}

f3b707d7b41594e959099efe190b76bc5915e08b
medchek <wizzsha@gmail.com>
2023-10-08 18:47:28 +0100

    style: update account actions colors

--- src/components/AccountActions.tsx
+++ src/components/AccountActions.tsx
@@ -18,7 +18,7 @@ export default function AccountActions() {
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger
-        className="flex h-11 w-11 items-center justify-center overflow-hidden rounded-full dark:bg-stone-900 dark:hover:bg-stone-800"
+        className="flex h-11 w-11 items-center justify-center overflow-hidden rounded-full dark:bg-neutral-900 dark:hover:bg-neutral-800"
         disabled={status !== "authenticated"}
       >
         <div className="relative flex h-9 w-9 items-center justify-center overflow-hidden rounded-full">
@@ -48,10 +48,10 @@ export default function AccountActions() {
         avoidCollisions
         sideOffset={4}
         align="end"
-        className="z-10 flex flex-col gap-1 rounded-md border border-stone-800 bg-stone-900 p-1 text-sm text-stone-400 outline-none"
+        className="z-10 flex flex-col gap-1 rounded-md border border-neutral-800 bg-neutral-900 p-1 text-sm text-stone-400 outline-none"
       >
         <DropdownMenuItem
-          className="flex h-8 cursor-pointer items-center gap-1 rounded-md px-2 hover:bg-stone-800 hover:outline-none"
+          className="flex h-8 cursor-pointer items-center gap-1 rounded-md px-2 hover:bg-neutral-800 hover:outline-none"
           onClick={() => signOut()}
         >
           <IoLogOutOutline className="h-6 w-6" /> <span>Se déconncter</span>

8ea46d5b359bc985f12957b4acdd4bd410b8a2bf
medchek <wizzsha@gmail.com>
2023-10-16 20:18:02 +0100

    feat: use tailwind dark mode class strategy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {

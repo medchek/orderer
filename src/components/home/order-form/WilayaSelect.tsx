51a55070d73d7a35c21850ee478ca4b2d3e64842
medchek <wizzsha@gmail.com>
2023-10-17 17:04:15 +0100

    fix: wilaya select default value not properly read

--- src/components/home/order-form/WilayaSelect.tsx
+++ src/components/home/order-form/WilayaSelect.tsx
@@ -1,45 +1,59 @@
 "use client";
-import React, { SelectHTMLAttributes } from "react";
-import { RegisterOptions, UseFormRegister } from "react-hook-form";
+import React, { SelectHTMLAttributes, useEffect } from "react";
+import {
+  Path,
+  PathValue,
+  RegisterOptions,
+  UseFormRegister,
+  UseFormSetValue,
+} from "react-hook-form";
 import { useState } from "react";
 import { useStore } from "@/store";
 import { SHIPPING_TYPE } from "@/store/orderFormSlice";
 import SelectInput from "./SelectInput";
-import { OrderFormValues } from "./OrderForm";
 import { useGetWilayas } from "@/features/shipping-prices/api/getWilayas";
+import { toPositiveNumber } from "@/lib/utils";
 
-
-interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
-  label: string;
+interface Props<T extends { wilaya: string }>
+  extends SelectHTMLAttributes<HTMLSelectElement> {
   id: string;
-  register: UseFormRegister<OrderFormValues>;
+  register: UseFormRegister<T>;
   registerRules?: RegisterOptions;
+  /** set the default value of a react-hook-form form using the setValue function. Uses the defaultValue value. */
+  setFormHookValue?: UseFormSetValue<T>;
   error?: string;
+  hidePrice?: boolean;
+  defaultValue?: string | number;
 }
 
-export default function WilayaSelect({
+export default function WilayaSelect<T extends { wilaya: string }>({
   id,
   register,
+  hidePrice,
+  error,
+  registerRules,
+  defaultValue,
+  setFormHookValue,
   ...props
-}: Props) {
-  const { shippingType, setSelectedWilaya, confirmData } = useStore(
-    (state) => state
-  );
+}: Props<T>) {
+  const { shippingType, setSelectedWilaya } = useStore((state) => state);
 
-  const { isFetching, data } = useGetWilayas();
+  const { isFetching, data: wilayaData } = useGetWilayas();
 
   // used to track and set the default input value.
   // Needed to display the default message (value=0) acting as a placeholder
-  const [selectedValue, setSelectedValue] = useState(confirmData?.wilaya ?? 0);
+  const [selectedValue, setSelectedValue] = useState<string>("");
 
   const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     // update the register value on change
-    const wilayaCode = parseInt(e.target.value);
+    const wilayaCode = e.target.value;
     // set the selected wilaya value
     setSelectedValue(wilayaCode);
 
     // look for the wilaya
-    const targetWilaya = data?.find((w) => w.code === wilayaCode);
+    const targetWilaya = wilayaData?.find(
+      (w) => w.code === toPositiveNumber(wilayaCode),
+    );
     // set the price accordingly
     if (targetWilaya) {
       // save the selectedWilya in the store
@@ -47,20 +61,52 @@ export default function WilayaSelect({
     }
   };
 
+  useEffect(() => {
+    if (wilayaData) {
+      if (defaultValue) {
+        const defaultVal =
+          typeof defaultValue === "number"
+            ? defaultValue.toString()
+            : defaultValue;
+        const defaultWilaya = wilayaData?.find(
+          ({ code }) => code === toPositiveNumber(defaultVal),
+        );
+
+        if (defaultWilaya) {
+          setSelectedWilaya(defaultWilaya);
+          setSelectedValue(defaultVal);
+          if (setFormHookValue) {
+            setFormHookValue(
+              "wilaya" as Path<T>,
+              defaultVal as PathValue<T, Path<T>>,
+            );
+          }
+        }
+      }
+    }
+  }, [
+    wilayaData,
+    defaultValue,
+    setSelectedValue,
+    setSelectedWilaya,
+    setFormHookValue,
+  ]);
   const selectOptions = () => {
-    if (data) {
+    if (wilayaData) {
       return [
-        <option value={0} disabled hidden key="0">
+        <option value="" disabled hidden key="0">
           Selectionnez la wilaya de livraison
         </option>,
-        data.map((wilaya) => {
+        wilayaData.map((wilaya) => {
           return (
             <option value={wilaya.code} key={wilaya.code}>
-              {wilaya.code} - {wilaya.name} (
-              {shippingType === SHIPPING_TYPE.HOME
-                ? wilaya.homePrice
-                : wilaya.officePrice}
-              DA)
+              {wilaya.code} - {wilaya.name}
+              {!hidePrice &&
+                ` (${
+                  shippingType === SHIPPING_TYPE.HOME
+                    ? wilaya.homePrice
+                    : wilaya.officePrice
+                }DA)`}
             </option>
           );
         }),
@@ -69,15 +115,17 @@ export default function WilayaSelect({
   };
 
   return (
-    <SelectInput
+    <SelectInput<T>
       {...props}
       disabled={isFetching}
       isLoading={isFetching}
-      name="wilaya"
-      label="Wilaya de Livraison"
+      name={"wilaya" as Path<T>}
+      label="Wilaya de livraison"
+      error={error}
       register={register}
       registerRules={{
         required: "Aucune wilaya n'a été selectionnée",
+        ...registerRules,
         // validate: orderFormValidators.wilaya,
         onChange: handleOnChange,
       }}

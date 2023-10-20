844c39bea2cd5d5b59de8ed5ccda2a61edc433d0
medchek <wizzsha@gmail.com>
2023-10-20 16:57:39 +0100

    style: add attribte to change content class

--- src/components/dashboard/DashboardEmptyState.tsx
+++ src/components/dashboard/DashboardEmptyState.tsx
@@ -4,12 +4,15 @@ import DashboardEmptyStateButton from "./DashboardEmptyStateButton";
 
 interface Props {
   className?: string;
+  contentClassName?: string;
   Icon: React.ReactNode;
   text: string;
   subContent?: React.ReactNode | string;
   subContentAction?: () => void;
   /** Display a plus icon before the action button text */
   actionButtonIcon?: boolean;
+  /** By default, the component has a -y-translate-12 class. This prop can be use to disable it by default */
+  noTranslate?: boolean;
 }
 
 export default function DashboardEmptyState({
@@ -18,7 +21,9 @@ export default function DashboardEmptyState({
   text,
   subContentAction,
   actionButtonIcon,
+  noTranslate,
   className,
+  contentClassName,
 }: Props) {
   const subContentDisplay = () => {
     if (subContentAction !== undefined) {
@@ -33,7 +38,7 @@ export default function DashboardEmptyState({
 
     if (subContent) {
       if (typeof subContent === "string") {
-        return <p className="text-sm text-stone-400">{subContent}</p>;
+        return <p className="text-sm text-neutral-400">{subContent}</p>;
       } else {
         return subContent;
       }
@@ -42,11 +47,19 @@ export default function DashboardEmptyState({
   return (
     <div
       className={cn(
-        "flex h-full w-full -translate-y-12 items-center justify-center",
+        "flex h-full w-full items-center justify-center",
+        {
+          "-translate-x-12": !noTranslate,
+        },
         className,
       )}
     >
-      <div className="flex w-96 flex-col items-center gap-1 text-center  text-stone-100">
+      <div
+        className={cn(
+          "flex w-96 flex-col items-center gap-1 text-center  text-neutral-900 dark:text-neutral-100",
+          contentClassName,
+        )}
+      >
         {Icon}
         <p>{text}</p>
 

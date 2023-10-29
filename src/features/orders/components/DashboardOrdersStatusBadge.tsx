65cd99b97d2da2884aaae753d9f9ebaff5027298
medchek <wizzsha@gmail.com>
2023-10-29 17:12:31 +0100

    feat: add prop to alter badge bg color

--- src/features/orders/components/DashboardOrdersStatusBadge.tsx
+++ src/features/orders/components/DashboardOrdersStatusBadge.tsx
@@ -6,11 +6,14 @@ import React from "react";
 interface Props {
   status: Status;
   isLoading?: boolean;
+  /** Brighten the background color slightly to accomodate for the same bg color */
+  isOnSameBgColor?: boolean;
 }
 
 export default function DashboardOrdersStatusBadge({
   status,
   isLoading,
+  isOnSameBgColor,
 }: Props) {
   const badgeText = () => {
     switch (status) {
@@ -31,19 +34,23 @@ export default function DashboardOrdersStatusBadge({
   return (
     <div
       className={clsx(
-        "flex items-center justify-center px-2 h-7 text-sm bg-neutral-900 rounded-md gap-2",
+        "line-clamp-1 flex h-7 items-center justify-center gap-2 whitespace-nowrap rounded-md  px-2 text-xs 2xl:text-sm",
+        isOnSameBgColor
+          ? "bg-neutral-200/70 dark:bg-neutral-800"
+          : "bg-neutral-300 dark:bg-neutral-900",
         {
-          "text-neutral-400": status === Status.UNCONFIRMED,
+          "text-neutral-600 dark:text-neutral-400":
+            status === Status.UNCONFIRMED,
           "text-blue-500": status === Status.CONFIRMED,
           "text-teal-400": status === Status.SHIPPING,
           "text-emerald-500": status === Status.SUCCESS,
           "text-red-500": status === Status.CANCELED,
           "text-rose-700": status === Status.RETURNED,
-        }
+        },
       )}
     >
       {badgeText()}
-      {isLoading && <Loader className="w-4 h-4 border-neutral-600" />}
+      {isLoading && <Loader className="h-4 w-4 border-neutral-600" />}
     </div>
   );
 }

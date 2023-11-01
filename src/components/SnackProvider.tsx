fe9022a73225f9fd9ef97783c21e74d76bba431d
medchek <wizzsha@gmail.com>
2023-11-01 16:05:08 +0100

    feat: add provider for snackbar

---
+++ src/components/SnackProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useStore } from "@/store";
+const Snackbar = dynamic(() => import("./Snackbar"));
+
+export default function SnackProvider() {
+  const { isSnackShown } = useStore();
+
+  return isSnackShown && <Snackbar />;
+}

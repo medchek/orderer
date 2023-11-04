d0e8a9c18bd80c37aaa6c9099cda5a0f7958d748
medchek <wizzsha@gmail.com>
2023-11-04 11:36:03 +0100

    types: add settings feature types

---
+++ src/features/settings/types/index.d.ts
@@ -0,0 +1,13 @@
+import { GetAccountResponse } from "../api/getAccount";
+
+export interface SettingsAccountFormValues {
+  phone: string;
+  wilaya: string;
+  town: string;
+  fullName: string;
+  address: string;
+}
+
+export type AccountDetail = GetAccountResponse;
+
+export type ThemeType = "dark" | "light" | "sys";

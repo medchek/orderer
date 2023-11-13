3bc28f327dd078dd8068afdac8aa2378168e9c65
medchek <wizzsha@gmail.com>
2023-11-13 18:42:52 +0100

    types: simplify MutationOptions type usage

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,19 +1,15 @@
-import {
-  UseMutationOptions,
-  UseQueryOptions
-} from "@tanstack/react-query";
+import { UseMutationOptions, UseQueryOptions } from "@tanstack/react-query";
 import { HTTPError } from "ky";
 
 export type PromiseStatus = "init" | "fetching" | "success" | "error";
 
 export type MutationOptions<
   /** Returned data type  */
-  TData = unknown,
+  TReturnData = unknown,
   /** Payload type */
-  TVariables = void,
-  TContext = unknown,
+  TPayloadVariables = void,
 > = Omit<
-  UseMutationOptions<TData, HTTPError, TVariables, TContext>,
+  UseMutationOptions<TReturnData, HTTPError, TPayloadVariables, TContext>,
   "mutationFn"
 >;
 
@@ -21,4 +17,3 @@ export type QueryOptions<TReturnData = unknown> = Omit<
   UseQueryOptions<TReturnData, HTTPError, TReturnData>,
   "queryFn"
 >;
-

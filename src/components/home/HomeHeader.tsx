5ce515c6792193ca4b59a859ecbfd38851e17d6e
medchek <wizzsha@gmail.com>
2024-06-24 16:03:47 +0100

    feat: show number of products selected for order

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,13 +1,16 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import AccountActions from "../AccountActions";
 import Link from "next/link";
+import { useStore } from "@/store";
 
 interface Props {
   isAdmin?: boolean;
 }
 
-export default async function HomeHeader({ isAdmin }: Props) {
+export default function HomeHeader({ isAdmin }: Props) {
+  const { selectedProducts } = useStore();
   return (
     <header className="relative z-10 flex h-20 min-h-[5rem] w-full items-start justify-between pt-2">
       <Link className="relative h-12 w-12" href="..">
@@ -17,7 +20,18 @@ export default async function HomeHeader({ isAdmin }: Props) {
       <div className="flex items-center gap-4 text-sm">
         <nav className="hidden gap-4 lg:flex">
           <Link href="../" className="text-blue-500 hover:underline">
-            Commander
+            <span className="flex items-center gap-1">
+              <span>Commander</span>
+              {/* Show number of selected products */}
+              {selectedProducts.length > 0 ? (
+                <span
+                  title="Nombre de produits sélectionnés pour livraison"
+                  className="flex size-5 items-center justify-center rounded-md bg-blue-500 font-medium text-neutral-50"
+                >
+                  {selectedProducts.length}
+                </span>
+              ) : null}
+            </span>
           </Link>
           <Link href="../products" className="text-neutral-500 hover:underline">
             Produits

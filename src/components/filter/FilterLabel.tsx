16d865eb9a20d26587c339d6481965418c42c41f
medchek <wizzsha@gmail.com>
2024-06-24 16:12:48 +0100

    feat: allow for vertical display of filter label content

--- src/components/filter/FilterLabel.tsx
+++ src/components/filter/FilterLabel.tsx
@@ -3,19 +3,34 @@ import React from "react";
 
 interface Props extends React.LabelHTMLAttributes<HTMLLabelElement> {
   label: string;
+  column?: boolean;
   children: React.ReactNode;
 }
 
 export default function FilterLabel({
   label,
   children,
+  column,
   className,
   ...props
 }: Props) {
   return (
-    <div className="flex items-center justify-between gap-6 lg:gap-0">
+    <div
+      className={cn("flex", {
+        "flex-row items-center justify-between gap-6 lg:gap-0": !column,
+        "flex-col items-start gap-2": column,
+      })}
+    >
       <label {...props}>{label}</label>
-      <div className={cn("relative flex items-center", className)}>
+      <div
+        className={cn(
+          "relative flex items-center",
+          {
+            "w-full": column,
+          },
+          className,
+        )}
+      >
         {children}
       </div>
     </div>

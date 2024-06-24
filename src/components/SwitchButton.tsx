1ff6b572f68813c08c24a565eb3c52cb85edb5be
medchek <wizzsha@gmail.com>
2024-06-24 16:06:04 +0100

    style: prevent width shrink for switch button by default

--- src/components/SwitchButton.tsx
+++ src/components/SwitchButton.tsx
@@ -8,14 +8,14 @@ export default function SwitchButton({ isActive, ...props }: Props) {
   return (
     <button
       type="button"
-      className={`relative flex h-7 w-14 items-center  rounded-full transition-colors ${
-        isActive ? "bg-secondary" : " bg-stone-700"
+      className={`relative flex h-7 w-14 min-w-[3.5rem] items-center rounded-full transition-colors ${
+        isActive ? "bg-secondary" : "bg-stone-700"
       }`}
       {...props}
     >
       <span
         className={`absolute inline-block h-6 w-6 rounded-full transition-all ${
-          isActive ? " translate-x-3 bg-white" : "-translate-x-3 bg-stone-300"
+          isActive ? "translate-x-3 bg-white" : "-translate-x-3 bg-stone-300"
         } `}
       ></span>
     </button>

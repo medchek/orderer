844866d26cbac3e286c2c836131d87a962447219
medchek <wizzsha@gmail.com>
2024-06-24 15:53:05 +0100

    feat: update react-query client to allow for SSR

--- src/lib/reactQuery.ts
+++ src/lib/reactQuery.ts
@@ -1,11 +1,33 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, isServer } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 30,
-      gcTime: 1000 * 60 * 60,
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60 * 30,
+        gcTime: 1000 * 60 * 60,
+      },
     },
-  },
-});
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export const queryClient = getQueryClient();

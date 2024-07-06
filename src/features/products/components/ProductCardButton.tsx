0ae38fb54191749635ec36af8a0014aa1302c52a
medchek <wizzsha@gmail.com>
2024-07-06 19:32:57 +0100

    style: update dark hover color

--- src/features/products/components/ProductCardButton.tsx
+++ src/features/products/components/ProductCardButton.tsx
@@ -21,7 +21,7 @@ export default function ProductCardButton({
   return (
     <Button
       className={cn(
-        "flex h-7 w-full items-center justify-center rounded-md bg-neutral-200 text-sm font-semibold transition-colors hover:bg-gray-300 active:bg-secondary active:text-white dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-700 dark:active:bg-neutral-900 lg:h-8 lg:text-base",
+        "flex h-7 w-full items-center justify-center rounded-md bg-neutral-200 text-sm font-semibold transition-colors hover:bg-gray-300 active:bg-secondary active:text-white dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-700/80 dark:active:bg-neutral-900 lg:h-8 lg:text-base",
         className,
       )}
       {...props}

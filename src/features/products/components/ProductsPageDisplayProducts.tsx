645d3647e2b5b4139e3bbc9e168021f7bc67cd09
medchek <wizzsha@gmail.com>
2024-07-06 19:42:31 +0100

    feat: use of products empty state component instead of generic

--- src/features/products/components/ProductsPageDisplayProducts.tsx
+++ src/features/products/components/ProductsPageDisplayProducts.tsx
@@ -5,13 +5,10 @@ import { useStore } from "@/store";
 import ProductCardLoader from "@/components/ProductCardLoader";
 import ProductCard from "./ProductCard";
 import FetchError from "@/components/dashboard/DashboardFetchError";
-import EmptyState from "@/components/dashboard/DashboardEmptyState";
 import Pagination from "@/components/Pagination";
-import { MdOutlineFilterAltOff } from "react-icons/md";
-import { TbPackage } from "react-icons/tb";
-import useProductFilter from "../hooks/useProductFilter";
 import ProductCardButton from "@/features/products/components/ProductCardButton";
 import ProductQuantitySelector from "./ProductQuantitySelector";
+import ProductsDisplayEmptyState from "./ProductsDisplayEmptyState";
 
 /**
  * Component used to display paginated products list in products/page.tsx
@@ -19,14 +16,6 @@ import ProductQuantitySelector from "./ProductQuantitySelector";
  */
 export default function ProductsPageDisplayProducts() {
   const { productsFilters, selectedProducts } = useStore();
-  const { resetFilters } = useProductFilter();
-
-  const [hasFilters, setHasFilters] = useState<boolean>(false);
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { currentPage: _, ...rest } = productsFilters;
-    setHasFilters(!!Object.keys(rest).length);
-  }, [productsFilters]);
 
   // This useQuery could just as well happen in some deeper
   // child to <Posts>, data will be available immediately either way
@@ -46,30 +35,7 @@ export default function ProductsPageDisplayProducts() {
     }
     // Empty data state
     if (!data || data.count === 0) {
-      return (
-        <EmptyState
-          className="absolute pb-24"
-          noTranslate
-          Icon={<TbPackage className="size-20" />}
-          text={
-            hasFilters
-              ? "Aucun produit ne correspond aux filtres appliqués"
-              : "Aucun produit n'a été encore ajouté"
-          }
-          subContent={
-            <button
-              type="button"
-              className="flex h-10 items-center justify-center gap-1 rounded-lg px-2 font-semibold transition-colors dark:hover:bg-stone-900 dark:focus:bg-stone-900/70"
-              onClick={resetFilters}
-            >
-              {hasFilters ? (
-                <MdOutlineFilterAltOff className="h-6 w-6" />
-              ) : null}
-              {hasFilters ? "Réinitialiser les filtres" : null}
-            </button>
-          }
-        />
-      );
+      return <ProductsDisplayEmptyState className="absolute pb-52" />;
     }
 
     // success state

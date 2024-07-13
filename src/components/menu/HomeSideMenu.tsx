501012a1ea5c810ea1e5a107cb005022a96b19e4
medchek <wizzsha@gmail.com>
2024-07-13 15:51:10 +0100

    feat: hightlight drawer menu current route

--- src/components/menu/HomeSideMenu.tsx
+++ src/components/menu/HomeSideMenu.tsx
@@ -11,12 +11,15 @@ import {
 import Link from "next/link";
 import { TbMenuDeep, TbSettings } from "react-icons/tb";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { LuLogIn, LuLogOut } from "react-icons/lu";
+import { LuLogIn, LuLogOut, LuMoonStar } from "react-icons/lu";
 import { BsBoxSeam, BsListNested } from "react-icons/bs";
 import AppAccountImage from "../AppAccountImage";
 import { signOut, useSession } from "next-auth/react";
 import HeaderSelectedProductsCounter from "../home/HeaderSelectedProductsCounter";
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import ThemeSelectInput from "./ThemeSelectInput";
 
 // const LazySheetContent = dynamic(
 //   () => import("../ui/Sheet").then((cmp) => cmp.SheetContent),
@@ -35,9 +38,9 @@ type MenuItemsData = {
 }[];
 
 // TODO: Lazy loading
-
 export default function HomeSideMenu() {
   const sessionData = useSession();
+  const pathname = usePathname();
 
   const menuItemsData: MenuItemsData = [
     {
@@ -72,13 +75,27 @@ export default function HomeSideMenu() {
     ({ Icon, text, href, onClick, Suffix }) => (
       <Link
         key={text}
-        className="flex h-12 w-full items-center justify-between rounded-md px-2 focus:dark:bg-neutral-800 focus:dark:text-neutral-50"
+        className={clsx(
+          "flex h-12 w-full items-center justify-between rounded-md px-2 active:bg-neutral-300 focus:dark:text-neutral-50 active:dark:bg-neutral-800",
+          {
+            "bg-neutral-300/40 dark:bg-[#050505] dark:text-neutral-50":
+              pathname === href || (pathname === "/" && href === ".."),
+          },
+        )}
         href={href}
         onClick={onClick}
       >
         <span className="flex w-full items-center gap-5">
           <Icon className="size-6" />
-          <span className="font-medium">{text}</span>
+          <span
+            className={clsx(
+              pathname === href || (pathname === "/" && href === "..")
+                ? "font-semibold"
+                : "font-medium",
+            )}
+          >
+            {text}
+          </span>
         </span>
         {Suffix}
       </Link>
@@ -94,7 +111,7 @@ export default function HomeSideMenu() {
       </SheetTrigger>
       <SheetContent
         autoFocus={false}
-        className="flex w-[80%] flex-col rounded-l-lg border-none bg-neutral-950 px-4 py-4"
+        className="flex w-[80%] flex-col rounded-l-lg border-none bg-neutral-200 px-4 py-4 dark:bg-neutral-950"
         side="left"
       >
         <SheetHeader>
@@ -106,36 +123,50 @@ export default function HomeSideMenu() {
         </SheetHeader>
 
         <div className="flex h-full w-full flex-col justify-between pt-6">
-          <div className="flex grow flex-col gap-2 text-neutral-300">
+          <div className="flex grow flex-col gap-2 text-neutral-800 dark:text-neutral-300">
             {displayMenuItems}
           </div>
 
-          <div
-            id="menu-account-display"
-            className="flex h-16 min-h-16 w-full items-center justify-between rounded-md bg-neutral-900 px-4"
-          >
-            <div className="flex items-center gap-4">
-              <AppAccountImage session={sessionData} />
-              <span>{sessionData.data?.user?.name}</span>
+          <section id="side-menu-bottom" className="flex flex-col gap-2">
+            <div
+              id="side-menu-theme-selector"
+              className="flex h-12 w-full items-center justify-between rounded-md px-2"
+            >
+              <div className="flex items-center gap-5 font-medium">
+                <LuMoonStar className="size-6" />
+                <span>Thème</span>
+              </div>
+
+              <ThemeSelectInput />
             </div>
-            {sessionData.status === "authenticated" ? (
-              <button
-                type="button"
-                className="size-9 rounded-md active:dark:bg-neutral-950"
-                title="Se déconnecter"
-              >
-                <LuLogOut className="size-6" />
-              </button>
-            ) : (
-              <Link
-                className="flex size-9 items-center justify-center rounded-md active:dark:bg-neutral-950"
-                title="Se connecter"
-                href="/login"
-              >
-                <LuLogIn className="size-6" />
-              </Link>
-            )}
-          </div>
+
+            <div
+              id="menu-account-display"
+              className="flex h-16 min-h-16 w-full items-center justify-between rounded-md bg-neutral-300 px-4 dark:bg-neutral-900"
+            >
+              <div className="flex items-center gap-4">
+                <AppAccountImage session={sessionData} />
+                <span>{sessionData.data?.user?.name}</span>
+              </div>
+              {sessionData.status === "authenticated" ? (
+                <button
+                  type="button"
+                  className="size-9 rounded-md active:dark:bg-neutral-950"
+                  title="Se déconnecter"
+                >
+                  <LuLogOut className="size-6" />
+                </button>
+              ) : (
+                <Link
+                  className="flex size-9 items-center justify-center rounded-md active:dark:bg-neutral-950"
+                  title="Se connecter"
+                  href="/login"
+                >
+                  <LuLogIn className="size-6" />
+                </Link>
+              )}
+            </div>
+          </section>
         </div>
       </SheetContent>
     </Sheet>

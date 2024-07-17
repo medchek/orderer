49f51ed1fb13d41b67fb88ca20a936eeb4270ef9
medchek <wizzsha@gmail.com>
2024-07-17 14:32:57 +0100

    feat: add more products seed data

--- prisma/seeders/productsSeedData.ts
+++ prisma/seeders/productsSeedData.ts
@@ -1,17 +1,16 @@
 // import { PRODUCT_CODE_LENGTH } from "../../src/lib/constants";
 // import { uniqueId } from "@/lib/utils";
+import { PRODUCT_CODE_LENGTH } from "@/lib/constants";
+import { uniqueId } from "@/lib/utils";
 import { Prisma } from "@prisma/client";
 
 // 1aF8dEnx-7d3oAuJ5j1SNhe4xwnl2b5D2 : air max 95
 // 1DsWxW2uZcK5shlI_QI7fL5HI0q_pJaTJ : power bank
 
 // static products codes are used to seed dummy orders
-const productCodes = [
-  "vvXh0TgpuWi48ilBKJr4",
-  "bq6ucPgr5bGASEhbJoy1",
-  "pE4XqhRMls7w9X0gZo7T",
-  "oNMzRxP2HyatmO6FYTIO",
-];
+const productCodes = Array.from({ length: 5 }, () =>
+  uniqueId(PRODUCT_CODE_LENGTH),
+);
 
 export const dummyProductData: Prisma.ProductUncheckedCreateInput[] = [
   {
@@ -79,6 +78,27 @@ export const dummyProductData: Prisma.ProductUncheckedCreateInput[] = [
         {
           // id: "1wPO_70ZdPA5VPIy2eLl068onrwxvDvZI",
           id: "orderer/o0njk8ffbapfttmugvao",
+        },
+      ],
+    },
+  },
+  {
+    code: productCodes[4],
+    name: "Air max 95 Triple Black",
+    description: "Couleur: Black",
+    price: 31000,
+    stock: 1,
+    discount: 0,
+    images: {
+      create: [
+        {
+          id: "orderer/eho7uy7prbmn0wax7d0s",
+        },
+        {
+          id: "orderer/tgnpbhgtlaoab6qtoilz",
+        },
+        {
+          id: "orderer/sym1vnoia2a1vdpoemfn",
         },
       ],
     },
